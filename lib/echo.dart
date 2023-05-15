1521945009b15b79c5c2f1b6af11cf5935530e5d
vsevolod <vsevex@gmail.com>
2023-05-15 11:50:04 +0400

    export `echo`

--- lib/echo.dart
+++ lib/echo.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library echo;
 
+export 'src/builder.dart';
 export 'src/echo.dart';
-
-// TODO: Export any libraries intended for clients of this package.
