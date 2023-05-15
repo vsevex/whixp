527533275386b8ebe5d267787c83ae7fb8aa607f
vsevolod <vsevex@gmail.com>
2023-05-15 11:49:53 +0400

    feat(builder): breaking change to main class

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -10,24 +10,38 @@ class EchoBuilder {
   /// Creates a new instance of the [EchoBuilder] class with the given name
   /// and optional attributes.
   EchoBuilder(
+    /// [String] representation of the name of an XML element that is being
+    /// constructed by the builder.
     this.name, [
-    Map<String, String>? attributes,
+    /// [Map] representation of attribute key-value pairs for the XML element
+    /// being constructed.
+    this.attributes = const {},
   ]) {
     /// Sets correct namespace for jabber:client elements.
     if (name == 'presence' || name == 'message' || name == 'iq') {
-      if (attributes != null && !attributes.containsKey('xlmns')) {
-        this.attributes!['xlmns'] = ns['CLIENT']!;
-      } else if (attributes == null) {
-        this.attributes = {'xlmns': ns['CLIENT']!};
+      if (!attributes.containsKey('xmlns')) {
+        attributes['xmlns'] = ns['CLIENT'];
       }
     }
 
     /// Holds the tree being built.
-    nodeTree = Utils.xmlElement(name, attributes: this.attributes);
+    nodeTree = Utils.xmlElement(name, attributes: attributes);
 
     /// Points to the current operation node.
     node = nodeTree;
   }
+
+  /// Creates an [EchoBuilder] with a <message/> element as the root.
+  factory EchoBuilder.message({Map<String, dynamic> attributes = const {}}) =>
+      EchoBuilder('message', attributes);
+
+  /// Creates an [EchoBuilder] with an <iq/> element as the root.
+  factory EchoBuilder.iq({Map<String, dynamic> attributes = const {}}) =>
+      EchoBuilder('iq', attributes);
+
+  /// Creates an [EchoBuilder] with a <presence/> element as the root.
+  factory EchoBuilder.pres({Map<String, dynamic> attributes = const {}}) =>
+      EchoBuilder('presence', attributes);
 
   /// [String] representation of the name of an XML element that is being
   /// constructed by the builder.
@@ -35,15 +49,15 @@ class EchoBuilder {
 
   /// [Map] representation of attribute key-value pairs for the XML element
   /// being constructed.
-  Map<String, String>? attributes;
+  final Map<String, dynamic> attributes;
 
-  xml.XmlNode? nodeTree;
+  xml.XmlElement? nodeTree;
   xml.XmlNode? node;
 
   /// This function returns a String serialiation of the current DOM tree. It is
   /// often used internally to pass data to a Request object.
   @override
-  String toString() => Utils.serialize(nodeTree! as xml.XmlElement)!;
+  String toString() => Utils.serialize(nodeTree)!;
 
   /// Sets the current node to its parent node.
   void up() => node = node!.parent;
@@ -55,7 +69,9 @@ class EchoBuilder {
   /// It takes a [Map] of key-value pairs as an argument, which are iterated
   /// over, and each attribute is either added or modified in the current
   /// element based on whether the key exists or not in the Map.
-  void addAttributes(Map<String, String> attributes) {
+  ///
+  /// * @return [EchoBuilder] object.
+  EchoBuilder addAttributes(Map<String, String> attributes) {
     /// Iterates all attribute in the attributes [Map].
     for (final attribute in attributes.keys) {
       /// Checks if attributes contain key for verification.
@@ -68,12 +84,15 @@ class EchoBuilder {
         }
       }
     }
+    return this;
   }
 
   /// Adds a child element to the current element being built. It takes the
   /// child element's name as the first argument and an optional `attributes`
   /// Map and `text` string.
-  void c(
+  ///
+  /// * @return [EchoBuilder] object.
+  EchoBuilder c(
     String name, {
     Map<String, String>? attributes,
     String? text,
@@ -83,32 +102,36 @@ class EchoBuilder {
 
     /// Add created child to nodes.
     node!.children.add(child!);
-    if (text is! String && text is! num) {
+    if (text.runtimeType != String) {
       node = child;
     }
-    return;
+    return this;
   }
 
   /// This method is similar to the method of `c` method, but instead of
   /// passing the name and attributes, it takes an existing `xml.XmlElement`
   /// object and adds it as a child to the current element being built.
-  void cnode(xml.XmlElement element) {
+  ///
+  /// * @return [EchoBuilder] object.
+  EchoBuilder cnode(xml.XmlElement element) {
     final node = Utils.copyElement(element);
     this.node!.children.add(node);
     this.node = node;
-    return;
+    return this;
   }
 
   /// Add a child text element.
   ///
   /// This does not make the child the new current element since there are no
   /// children of text elements.
-  void t(String text) {
+  ///
+  /// * @return [EchoBuilder] object.
+  EchoBuilder t(String text) {
     /// Create text node.
     final child = Utils.xmlTextNode(text);
 
     /// Add created text node to current nodes.
     node!.children.add(child);
-    return;
+    return this;
   }
 }
