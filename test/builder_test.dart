a2b9cb4c05410741b640aaed1e70702a2ddaab71
vsev <vsevex@gmail.com>
2023-05-19 19:06:48 +0400

    feat(test): create builder test(s)

--- test/builder_test.dart
+++ test/builder_test.dart
@@ -41,4 +41,49 @@ void main() {
       expect(generated, equals(expected));
     });
   });
+
+  group('up Method Test', () {
+    test(
+      'Must return valid builder object with a single child element',
+      () {
+        final builder = EchoBuilder('parent');
+        builder.c('child', attributes: {});
+        builder.up();
+        final result = builder.nodeTree.toString();
+        expect(result, equals('<parent><child/></parent>'));
+      },
+    );
+
+    test(
+      'Must return valid tree string with multiple child elements',
+      () {
+        final builder = EchoBuilder('parent');
+        builder.c('child1').up().c('child2', attributes: {});
+        final result = builder.nodeTree.toString();
+        expect(result, equals('<parent><child1/><child2/></parent>'));
+      },
+    );
+
+    test(
+      'Must return valid node tree string with multiple levels of nesting',
+      () {
+        final builder = EchoBuilder('parent');
+        builder
+            .c('child1')
+            .c('grandchild1')
+            .c('greatgrandchild1')
+            .up()
+            .up()
+            .up()
+            .c('child2', attributes: {});
+        final result = builder.nodeTree.toString();
+        expect(
+          result,
+          equals(
+            '<parent><child1><grandchild1><greatgrandchild1/></grandchild1></child1><child2/></parent>',
+          ),
+        );
+      },
+    );
+  });
 }
