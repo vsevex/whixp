f6f02138776fadd0da5048326263727bc60020e7
vsevolod <vsevex@gmail.com>
2023-05-22 01:23:45 +0400

    feat(test): write scram test for all scram methods

--- test/scram_test.dart
+++ test/scram_test.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 import 'package:convert/convert.dart' as convert;
 
 import 'package:echo/echo.dart';
@@ -7,16 +5,16 @@ import 'package:echo/echo.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('hmacIterations Method Test', () {
+  group('hmacIteration Method Test', () {
     test('Must return correct decoded value in the output', () {
       final result = Scram.hmacIteration(
         key: 'password',
-        salt: Utils.stringToArrayBuffer('salt'),
+        salt: 'salt',
         iterations: 4096,
       );
       expect(
-        convert.hex.encode(result),
-        equals('4b007901b765489abead49d926f721d065a429c1'),
+        result.codeUnits,
+        equals(convert.hex.decode('4b007901b765489abead49d926f721d065a429c1')),
       );
     });
   });
@@ -25,14 +23,13 @@ void main() {
     test('Must return correct client key value in the output', () {
       final result = Scram().deriveKeys(
         password: 'pencil',
-        salt: Utils.base64ToArrayBuffer('QSXCR+Q6sek8bf92'),
+        salt: 'QSXCR+Q6sek8bf92',
         hashName: 'SHA-1',
         iterations: 4096,
-        hashBits: 160,
       );
       expect(
-        convert.hex.encode(result['ck']!),
-        equals('d53eaaf3c20aeb4e18ecdc0faa9b212967b0be73'),
+        convert.hex.encode(result['sk']!.codeUnits),
+        equals('5cdfcf5896307930d4d49260c9f55a532689471e'),
       );
     });
   });
@@ -43,32 +40,12 @@ void main() {
           'n=user,r=fyko+d2lbbFgONRv9qkxdawL,r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j,s=QSXCR+Q6sek8bf92,i=4096,c=biws,r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j';
       final result = Scram.clientProof(
         message,
-        'SHA-1',
-        Uint8List.fromList(
-          convert.hex.decode('d53eaaf3c20aeb4e18ecdc0faa9b212967b0be73'),
-        ),
-      );
-      expect(
-        convert.hex.encode(result),
-        equals('0d33cbbce5519e0225ae0dcc574ce8e6ec5998e9'),
-      );
-    });
-  });
-
-  group('serverSign Method Test', () {
-    test('Must return correct server proof as a result', () {
-      const message =
-          'n=user,r=fyko+d2lbbFgONRv9qkxdawL,r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j,s=QSXCR+Q6sek8bf92,i=4096,c=biws,r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j';
-      final result = Scram().serverSign(
-        message,
-        Uint8List.fromList(
-          convert.hex.decode('018625aff475ec70628717c5e2b5aa9da2de0a91'),
-        ),
+        'ÀûQ¨êÇÝKÞÅí',
         'SHA-1',
       );
       expect(
-        convert.hex.encode(result),
-        equals('f093bd2aef3dd8c65e49ffca809efdb66b62affa'),
+        Utils.btoa(result),
+        equals('frsVRm77a2tPQ5vy+zZuaKRR17o='),
       );
     });
   });
@@ -110,16 +87,4 @@ void main() {
       expect(result['iter'], equals(4096));
     });
   });
-
-  // group('clientProof Method Test', () {
-  //   test('Must return expected result', () {
-  //     print(
-  //       Scram.clientProof(
-  //         'n=user,r=123456789,s=abcdefg,c=biws,r=fFj12oE6Zwv/fGHP8waZqGtkzug=',
-  //         'SHA-256',
-  //         Utils.stringToArrayBuffer('pencil'),
-  //       ),
-  //     );
-  //   });
-  // });
 }
