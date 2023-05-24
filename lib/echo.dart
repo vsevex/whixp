65cd75c46b4fb14f21430df8b2288e97e288858a
vsev <vsevex@gmail.com>
2023-05-24 12:55:10 +0400

    export `enums`

--- lib/echo.dart
+++ lib/echo.dart
@@ -1,5 +1,7 @@
+/// Imports [EchoBuilder], [Echo] main class, enums and [Echotils]
 library echo;
 
 export 'src/builder.dart';
 export 'src/echo.dart';
+export 'src/enums.dart';
 export 'src/utils.dart';
