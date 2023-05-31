5294d9ce3d6ef460a891f53844018c856c58c804
vsev <vsevex@gmail.com>
2023-05-31 16:38:03 +0400

    create initial connection `example`

--- example/echo_example.dart
+++ example/echo_example.dart
@@ -1,1 +1,18 @@
-void main() {}
+import 'dart:developer';
+
+import 'package:echo/echo.dart';
+
+Future<void> main() async {
+  final echo = Echo(service: 'ws://localhost:7070/ws');
+  await echo.connect(
+    jid: 'vsevex@localhost',
+    password: 'somepsw',
+    callback: (status) async {
+      if (status == EchoStatus.connected) {
+        log('Connection Established');
+      } else if (status == EchoStatus.disconnected) {
+        log('Connection Terminated');
+      }
+    },
+  );
+}
