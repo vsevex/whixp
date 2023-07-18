51814997a997c98c15df81b7c6f665e5a3ff7c2b
vsevolod <vsevex@gmail.com>
2023-07-18 04:14:21 +0400

    feat(extension): pre-release `registration`

--- lib/extensions/registration/registration_extension.dart
+++ lib/extensions/registration/registration_extension.dart
@@ -1,6 +1,10 @@
 part of '../../src/echo.dart';
 
+/// Implements the XEP-0077 (In-Band Registration) protocol. This extension
+/// allows users to register new accounts on an XMPP server directly from the
+/// client.
 class RegistrationExtension extends Extension {
+  /// Creates a new instance of [RegistrationExtension].
   RegistrationExtension() : super('registration-extension');
 
   /// This method is not implemented and will not be affected in the use of this
@@ -20,10 +24,26 @@ class RegistrationExtension extends Extension {
   void initialize(Echo echo) {
     super.echo = echo;
 
+    /// Initializes required namespaces that will be used on the registration
+    /// process, and adds the required feature for the extension to work
+    /// correctly.
     super.echo!.addNamespace('REGISTER', 'jabber:iq:register');
     super.echo!.disco.addFeature(ns['REGISTER']!);
   }
 
+  /// Initiates the registration process and sending an IQ stanza with the
+  /// necessary registration data to the XMPP server.
+  ///
+  /// Listens for the response and processes the received data to handle the
+  /// registration result.
+  ///
+  /// * @param resultCallback A callback function that will be invoked when
+  /// there is incoming `result` stanza from the server.
+  /// * @param errorCallback A callback function that will be invoked when there
+  /// is incoming `error` stanza from the server.
+  ///
+  /// For the proper usage of this method, please refer to the Readme file
+  /// associated with the extension folder.
   void submit({
     FutureOr<void> Function(xml.XmlElement)? resultCallback,
     FutureOr<void> Function(EchoException)? errorCallback,
@@ -38,6 +58,7 @@ class RegistrationExtension extends Extension {
       query.c(name).t(value!).up();
     }
 
+    /// Add system handler for accepting incoming stanzas.
     super.echo!._addSystemHandler(
       (stanza) {
         final query = stanza.findAllElements('query');
@@ -55,11 +76,11 @@ class RegistrationExtension extends Extension {
         }
         return false;
       },
-      name: 'iq',
       resultCallback: resultCallback,
       errorCallback: errorCallback,
     );
 
+    /// Send stanza which built using [EchoBuilder.iq] constructor.
     super.echo!.sendIQ(element: query.nodeTree!);
   }
 }
