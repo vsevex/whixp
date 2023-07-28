00075faab3d36ea6a803fa71ced2ab573ceadef5
vsev <vsevex@gmail.com>
2023-07-28 19:00:48 +0400

    feat(ext): document `caps` extension

--- lib/extensions/caps/caps_extension.dart
+++ lib/extensions/caps/caps_extension.dart
@@ -1,15 +1,35 @@
 import 'package:echo/echo.dart';
 import 'package:echo/src/constants.dart';
 
+/// Represents Caps (short for Entity Capabilities) plugin.
+///
+/// CapsExtension is an implementation of the [Extension] class that provides
+/// support for handling capabilities in the XMPP protocol using the
+/// Capabilities feature.
+///
+/// This extension allows the client to advertise its capabilities to other
+/// entities and discover the capabilities of other entities.
 class CapsExtension extends Extension {
+  /// Creates an instance of the [CapsExtension] with the provided extension
+  /// name.
+  ///
+  /// For more information about this extension please refer to [CapsExtension]
+  /// or `Readme`.
   CapsExtension() : super('caps-extension');
 
-  final _verificationString = '';
+  /// A [Map] to store known capabilities of other entities identified by the
+  /// `verification` attribute received in the presence stanzas.
   final _knownCapabilities = <String, List<Map<String, dynamic>>>{};
+
+  /// A [Map] to store the `verification` attribute received in the presence
+  /// from different entities, indexed by their respective JIDs.
   final _jidIndex = <String, String>{};
 
+  /// Called when the connection status changes.
   @override
   void changeStatus(EchoStatus status, String? condition) {
+    /// If the status is in `connected` state, it sends the client's presence
+    /// with its capabilities.
     if (status == EchoStatus.connected) {
       sendPresence();
     }
@@ -17,10 +37,14 @@ class CapsExtension extends Extension {
 
   @override
   void initialize(Echo echo) {
+    /// CAPS feature namespace
     echo.addNamespace('CAPS', 'http://jabber.org/protocol/caps');
 
+    /// Add the `CAPS` feature and client identity to the disco extension.
     echo.disco.addFeature(ns['CAPS']!);
     echo.disco.addIdentity(category: 'client', type: 'mobile', name: 'echo');
+
+    /// Set up presence handler to process the incoming presence stanzas.
     echo.addHandler(
       (stanza) {
         final from = stanza.getAttribute('from');
@@ -29,6 +53,8 @@ class CapsExtension extends Extension {
         final node = c.getAttribute('node');
 
         if (!_knownCapabilities.containsKey(ver)) {
+          /// If the capabilities are not known, request capabilities from the
+          /// entity.
           return _requestCapabilities(to: from!, node: node!, ver: ver!);
         } else {
           _jidIndex[from!] = ver!;
@@ -45,6 +71,15 @@ class CapsExtension extends Extension {
     super.echo = echo;
   }
 
+  /// Requests capabilities from the given entity (identified by `to` JID) with
+  /// the provided `node` and `ver` attributes.
+  ///
+  /// * @param to The Jabber Identifier to indicate who the request is going to.
+  /// * @param node A Unique Identifier for the capabilities being queried. It
+  /// helps in distinguishing between different sets of caps provided by the
+  /// same entity.
+  /// * @param ver Stands for verification string and helps preventing poisoning
+  /// of entity capabilities information.
   Future<bool> _requestCapabilities({
     required String to,
     required String node,
@@ -57,6 +92,8 @@ class CapsExtension extends Extension {
     return true;
   }
 
+  /// Handles the reply to the disco#info query and updates the known
+  /// capabilities for the entity identified by 'from' JID.
   bool _handleDiscoInfoReply(XmlElement stanza) {
     final query = stanza.findAllElements('query').first;
     final node = query.getAttribute('node')!.split('#');
@@ -65,6 +102,8 @@ class CapsExtension extends Extension {
 
     if (!_knownCapabilities.containsKey(ver) ||
         _knownCapabilities[ver] == null) {
+      /// If the capabilities are not known, add them to the knownCapabilities
+      /// [Map].
       final nodes = query.descendantElements.toList();
       _knownCapabilities[ver] = [];
       for (int i = 0; i < nodes.length; i++) {
@@ -80,6 +119,8 @@ class CapsExtension extends Extension {
     return false;
   }
 
+  /// Generates the attributes for the 'c' (capabilities) node in the client's
+  /// presence.
   Map<String, String> get _generateCapsAttributes => {
         'xmlns': ns['CAPS']!,
         'hash': 'sha-1',
@@ -87,16 +128,18 @@ class CapsExtension extends Extension {
         'ver': _generateVerificationString,
       };
 
+  /// Creates the 'c' (capabilities) node for the client's presence.
   XmlElement? get _createCapsNode =>
       EchoBuilder('c', _generateCapsAttributes).nodeTree;
 
   void sendPresence() => echo!.send(EchoBuilder.pres().cnode(_createCapsNode!));
 
+  /// Generates the verification string for the client's capabilities based on
+  /// the identities and features supported by the client.
+  ///
+  /// For more information about this string please refer to the documentation:
+  /// https://xmpp.org/extensions/xep-0115.html#ver
   String get _generateVerificationString {
-    if (_verificationString.isNotEmpty) {
-      return _verificationString;
-    }
-
     final verificationStringBuffer = StringBuffer();
     final identities = echo!.disco.identities;
     _sort(identities, 'category');
@@ -123,6 +166,9 @@ class CapsExtension extends Extension {
     );
   }
 
+  /// Sorts the list of [DiscoIdentity] objects based on the specified
+  /// 'property'. The sorting is done in-place and returns the sorted list of
+  /// identities.
   List<DiscoIdentity> _sort(List<DiscoIdentity> identities, String property) {
     if (property == 'category') {
       identities.sort((i1, i2) => i1.category.compareTo(i2.category));
