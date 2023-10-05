ad16f4d43819facced67f9388ea5db92c215540c
vsevex <vsevex@gmail.com>
2023-10-05 21:33:00 +0400

    `string preparation` profiles class

--- lib/src/echotils/src/stringprep.dart
+++ lib/src/echotils/src/stringprep.dart
@@ -1,136 +1,23 @@
-import 'package:echox/echox.dart';
 import 'package:echox/src/stringprep/stringprep.dart';
-
-import 'package:unorm_dart/unorm_dart.dart' as unorm;
+import 'package:memoize/memoize.dart';
 
 class StringPreparationProfiles {
-  const StringPreparationProfiles();
+  final _nodePrep = StringPreparation().preps['nodeprep'];
+  final _resourcePrep = StringPreparation().preps['resourceprep'];
 
-  static String? b1Map(String char) =>
-      StringPreparation.inTableb1(char) ? '' : null;
-
-  static String? c12Map(String char) =>
-      StringPreparation.inTablec12(char) ? '' : null;
-
-  static String mapInput(
-    String data, [
-    List<String? Function(String)>? tables,
-  ]) {
-    final result = <String>[];
-
-    if (tables == null) {
-      return data;
-    }
-
-    for (int i = 0; i < data.length; i++) {
-      final char = data[i];
-      String? replacement;
-
-      for (final mapping in tables) {
-        replacement = mapping(char);
-        if (replacement != null) {
-          break;
-        }
-      }
-
-      if (replacement != null) {
-        replacement = char;
-      }
-      result.add(char);
-    }
-
-    return result.join();
+  String? nodePrep(String node) {
+    if (_nodePrep == null) return null;
+    final check = memo1((String node) => _nodePrep!(node));
+    return check(node);
   }
 
-  static String normalize(String data, {bool nkfc = true}) {
-    if (nkfc) {
-      return unorm.nfkc(data);
-    }
-    return data;
+  String? resourcePrep(String resource) {
+    if (_resourcePrep == null) return null;
+    final check = memo1((String resource) => _resourcePrep!(resource));
+    return check(resource);
   }
 
-  static bool prohibitOutput(
-    String data, [
-    List<bool Function(String)>? tables,
-  ]) {
-    if (tables == null) {
-      return false;
-    }
-
-    for (int i = 0; i < data.length; i++) {
-      final char = data[i];
-
-      for (final check in tables) {
-        if (check(char)) {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  }
-
-  static bool checkBidi(String data) {
-    if (data.isEmpty) {
-      return false;
-    }
-
-    bool hasLcat = false;
-    bool hasRandal = false;
-
-    for (int i = 0; i < data.length; i++) {
-      final char = data[i];
-
-      if (StringPreparation.inTablec8(char)) {
-        return false;
-      }
-      if (StringPreparation.inTabled1(char)) {
-        hasRandal = true;
-      } else if (StringPreparation.inTabled2(char)) {
-        hasLcat = true;
-      }
-    }
-
-    if (hasRandal && hasLcat) {
-      return false;
-    }
-
-    final firstRandal = StringPreparation.inTabled1(data[0]);
-    final lastRandal = StringPreparation.inTabled1(data[data.length - 1]);
-
-    if (firstRandal && !(firstRandal && lastRandal)) {
-      return false;
-    }
-
-    return true;
-  }
-
-  static String stringPreparationProfile({
-    bool nkfc = true,
-    bool bidi = true,
-    List<String? Function(String)>? mappings,
-    List<bool Function(String)>? prohibited,
-    List<bool Function(String)>? unassigned,
-  }) {
-    String profile(dynamic data, {bool query = false}) {
-      String text;
-      try {
-        text = Echotils.unicode(data);
-      } catch (error) {
-        throw ArgumentError('String preparation error occured');
-      }
-
-      String edited = mapInput(text, mappings);
-      edited = normalize(edited, nkfc: nkfc);
-      prohibitOutput(edited, prohibited);
-      if (bidi) {
-        checkBidi(edited);
-      }
-      if (query && unassigned != null) {
-        throw Exception();
-      }
-    }
-
-    return profile();
+  void idna(String domain) {
+    
   }
 }
