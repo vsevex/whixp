1321b6e133b7e95e3acdc20cbcc0d6622533a284
vsevolod <vsevex@gmail.com>
2023-10-06 04:01:05 +0400

    upgrade `idna` test

--- test/idna_test.dart
+++ test/idna_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:echox/src/echotils/src/idna.dart';
 import 'package:test/test.dart';
 
@@ -6,6 +8,29 @@ void main() {
     test('Case folding ASCII U+0043 U+0041 U+0046 U+0045', () {
       const original = 'CAFE';
       const expected = 'cafe';
+
+      final nameprop = IDNA.nameprep(original);
+      expect(nameprop, equals(expected));
+    });
+    test('Case folding 8bit U+00DF (german sharp s)', () {
+      const original = '\xC3\x9F';
+      const expected = 'ss';
+      final decoded = utf8.decode(original.codeUnits);
+
+      final nameprop = IDNA.nameprep(decoded);
+      expect(nameprop, equals(expected));
+    });
+    test('Case folding U+0130 (turkish capital I with dot)', () {
+      const original = '\xC4\xB0';
+      const expected = 'i\xcc\x87';
+      final decoded = utf8.decode(original.codeUnits);
+
+      final nameprop = IDNA.nameprep(decoded);
+      expect(nameprop, equals(expected));
+    });
+    test('Self-reverting case folding U+01F0 and normalization', () {
+      const original = '\xC7\xF0';
+      const expected = '\xC7\xB0';
 
       final nameprop = IDNA.nameprep(original);
       expect(nameprop, equals(expected));
