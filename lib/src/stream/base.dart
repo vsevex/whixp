5b24b2cee8798c7eef85e89c289efb9c7c4703dc
vsevolod <vsevex@gmail.com>
2023-10-16 04:22:43 +0400

    feat(stream): base xml streaming class decleration
    written several methods for this class. none of these methods are tested yet, but mainly it will be done by the next commit to this branch. todos are not marked, but test cases will be written too. documentation need to take place after test cases.

--- lib/src/stream/base.dart
+++ lib/src/stream/base.dart
@@ -6,36 +6,6 @@ import 'package:xml/xml.dart' as xml;
 
 typedef FilterCallback = bool Function(dynamic);
 
-dynamic _getAttribute(XMLBase stanza, String attribute) {
-  switch (attribute) {
-    case 'pluginAttributeMap':
-      return stanza.pluginAttributeMap;
-    case 'pluginTagMap':
-      return stanza.pluginTagMap;
-    case 'pluginIterables':
-      return stanza.pluginIterables;
-    case 'pluginOverrides':
-      return stanza.pluginOverrides;
-  }
-}
-
-void _setAttribute(XMLBase stanza, String attribute, dynamic value) {
-  switch (attribute) {
-    case 'pluginAttributeMap':
-      stanza.pluginAttributeMap = Map<String, XMLBase>.from(value as Map);
-      return;
-    case 'pluginTagMap':
-      stanza.pluginTagMap = Map<String, XMLBase>.from(value as Map);
-      return;
-    case 'pluginIterables':
-      stanza.pluginIterables = value as Set<XMLBase>;
-      return;
-    case 'pluginOverrides':
-      stanza.pluginOverrides = Map<String, String>.from(value as Map);
-      return;
-  }
-}
-
 // void registerStanzaPlugin(
 //   XMLBase stanza,
 //   XMLBase plugin, {
@@ -69,94 +39,75 @@ void _setAttribute(XMLBase stanza, String attribute, dynamic value) {
 //   }
 // }
 
-// class _Multi extends XMLBase {
-//   late Type _multiStanza;
+Either<String, List<String>> fixNamespace(
+  String xpath, {
+  bool split = false,
+  bool propogate = true,
+  String def = '',
+}) {
+  final fixed = <String>[];
+  final namespaceBlocks = xpath.split('{');
+  late List<String?> elements;
+  late String? namespace;
 
-//   @override
-//   bool setup([xml.XmlElement? element]) {
-//     this.element = xml.XmlElement(xml.XmlName(''));
-//     return false;
-//   }
+  for (final block in namespaceBlocks) {
+    if (block.contains('}')) {
+      final split = block.split('}');
+      namespace = split[0];
+      elements = split[1].split('/');
+    } else {
+      namespace = def;
+      elements = block.split('/');
+    }
 
-//   FilterCallback pluginFilter(List arguments) => (x) => x is XMLBase;
+    for (final element in elements) {
+      if (element != null && element.isNotEmpty) {
+        late String tag;
+        if (propogate && element[0] != '*') {
+          tag = '$namespace$element';
+        } else {
+          tag = element;
+        }
+        fixed.add(tag);
+      }
+    }
+  }
 
-//   FilterCallback pluginLanguageFilter(List arguments, [String? language]) =>
-//       (x) => x is XMLBase;
-
-//   @override
-//   dynamic noSuchMethod(Invocation invocation, {bool language = false}) {
-//     if (!invocation.isMethod || invocation.namedArguments.isNotEmpty) {
-//       super.noSuchMethod(invocation);
-//     }
-//     final arguments = invocation.positionalArguments;
-//     if (language) {
-//       return pluginFilter();
-//     }
-//     return pluginFilter(arguments);
-//   }
-
-//   XMLBase? _failWithoutParent() {
-//     XMLBase? parent;
-//     if (this.parent != null) {
-//       parent = this.parent;
-//     } else {
-//       throw Exception('No stanza parent for multifactory');
-//     }
-
-//     return parent;
-//   }
-
-//   List<XMLBase> get_multi({String? lang}) {
-//     final parent = _failWithoutParent();
-//     if (parent == null) return [];
-//     final res = lang == null || lang == '*'
-//         ? parent.where(pluginFilter())
-//         : parent.where(plugin_lang_filter(lang));
-
-//     return res.toList();
-//   }
-// }
-
-// XMLBase multifactory(XMLBase stanza, String pluginAttribute) {
-//   final multi = _Multi();
-
-//   multi.isExtension = true;
-//   multi.pluginAttribute = pluginAttribute;
-//   multi.multiStanza = stanza;
-//   multi.interfaces = {pluginAttribute};
-//   multi.languageInterfaces = {pluginAttribute};
-//   _setAttribute(multi, 'get$pluginAttribute', getMulti);
-//   _setAttribute(multi, 'set$pluginAttribute', setMulti);
-//   _setAttribute(multi, 'del$pluginAttribute', deleteAttribute);
-
-//   return multi;
-// }
+  if (split) return right(fixed);
+  return left(fixed.join());
+}
 
 class XMLBase {
   XMLBase({
     xml.XmlElement? element,
-    Either<WeakReference<XMLBase>, XMLBase>? parent,
+    WeakReference<XMLBase>? parent,
   }) {
+    _index = 0;
     name = 'stanza';
     namespace = Echotils.getNamespace('CLIENT');
+    pluginAttribute = 'plugin';
+    pluginMultiAttribute = '';
+    interfaces = <String>{'type', 'to', 'from', 'id', 'payload'};
+    subInterfaces = <String>{};
+    boolInterfaces = <String>{};
+    overrides = <String>[];
+    isExtension = false;
+    pluginOverrides = <String, String>{};
+    pluginAttributeMap = <String, XMLBase>{};
+    pluginTagMapping = <String, XMLBase>{};
+    iterables = <XMLBase>[];
+
     if (element != null) {
       this.element = element;
     }
 
-    parenT(parent);
+    if (parent != null) {
+      this.parent = parent.target;
+    }
 
     /// If XML generated, then everything is ready.
     if (setup(element)) {
       return;
-    }
-  }
-
-  void parenT(Either<WeakReference<XMLBase>, XMLBase>? parent) {
-    if (parent != null) {
-      parent.fold(
-        (reference) => this.parent = reference.target,
-        (parent) => this.parent = parent,
-      );
     }
   }
 
@@ -170,15 +121,28 @@ class XMLBase {
   /// is being used in an XMPP library.
   late String namespace;
 
+  late String pluginAttribute;
+
+  /// [XMLBase] subclasses that are intended to be an iterable group of items,
+  /// the `pluginMultiAttribute` value defines an interface for the parent
+  /// stanza which returns the entire group of matchihng substanzas.
+  late String pluginMultiAttribute;
+
   /// The set of keys that the stanza provides for accessing and manipulating
   /// the underlying XML object.
-  late Set<String> interfaces = <String>{'type', 'to', 'from', 'id', 'payload'};
+  late Set<String> interfaces;
 
+  /// A subset of `interfaces` which maps interfaces to direct subelements of
+  /// the underlying XML object. Using this set, the text of these subelements
+  /// may be set, retrieved, or removed without needing to define custom
+  /// methods.
+  late Set<String> subInterfaces;
+
+  /// A subset of `interfaces` which maps the presence of subelements to
+  /// boolean values. Using this set allows for quickly checking for the
+  /// existence of empty subelements like `<required />`.
+  late Set<String> boolInterfaces;
   late Set<String> languageInterfaces = <String>{};
-  late Map<String, XMLBase> pluginAttributeMap = {};
-  late Map<String, XMLBase> pluginTagMap = {};
-  late Set<XMLBase> pluginIterables = {};
-  late Map<Tuple2<String?, String?>, XMLBase> plugins;
 
   /// In some cases you may wish to override the behaviour of one of the
   /// parent stanza's interfaces. The `overrides` list specifies the interface
@@ -190,43 +154,46 @@ class XMLBase {
   /// ```
   ///
   /// Getting and deleting the `condition` interface would not be affected.
-  late List<String> overrides = [];
-  late Map<String, String> pluginOverrides = {};
-  late String pluginAttribute = 'plugin';
+  late List<String> overrides;
 
   /// If you need to add a new interface to an existing stanza, you can create
   /// a plugin and set `isExtension = true`. Be sure to set the
   /// `pluginAttribute` value to the desired interface name, and that it is the
   /// only interface listed in `interfaces`. Requests for the new interface
   /// from the parent stanza will be passed to the plugin directly.
-  late bool isExtension = false;
+  late bool isExtension;
 
-  /// [XMLBase] subclasses that are intended to be an iterable group of items,
-  /// the `pluginMultiAttribute` value defines an interface for the parent
-  /// stanza which returns the entire group of matchihng substanzas.
-  late String pluginMultiAttribute = '';
-  xml.XmlElement? element;
-  XMLBase? parent;
-  late final iterables = <XMLBase>[];
+  late Map<String, String> pluginOverrides;
+  late Map<String, XMLBase> pluginAttributeMap;
+
+  /// A mapping of root element tag names (in `$namespaceelementName` format)
+  /// to the plugin classes responsible for them.
+  late Map<String, XMLBase> pluginTagMapping;
+
+  /// The set of stanza classes that can be iterated over using
+  /// the `substanzas` interface.
+  late Set<XMLBase> pluginIterables = {};
+  late Map<Tuple2<String, String?>, XMLBase> plugins;
   late final loadedPlugins = <String>{};
 
+  /// The underlying [element] for the stanza.
+  late xml.XmlElement? element;
+  late final List<XMLBase> iterables;
+  late int _index;
+  late XMLBase? parent;
+
   bool setup([xml.XmlElement? element]) {
-    if (element != null && element != this.element) {
+    if (Echotils.getAttr(this, 'element') != null) {
+      return false;
+    }
+    if (Echotils.getAttr(this, 'element') == null && element != null) {
       this.element = element;
-      return false;
-    }
-
-    if (element != null) {
-      return false;
-    }
-
-    if (this.element != null) {
       return false;
     }
 
     for (final ename in name.split('/')) {
       final newXML = xml.XmlElement(xml.XmlName('$namespace$ename'));
-      if (this.element == null) {
+      if (Echotils.getAttr(this, 'element') == null) {
         this.element = newXML;
       } else {
         this.element!.children.add(newXML);
@@ -242,50 +209,40 @@ class XMLBase {
     return true;
   }
 
-  XMLBase? enable(String attribute, [String? language]) =>
-      initPlugin(attribute: attribute, language: language);
+  void enable() {}
 
   XMLBase? getPlugin(String name, [String? language, bool check = false]) {
-    String? languageTemp = language;
-    if (language != null) {
-      languageTemp = getLanguage();
-    }
+    late final lang = language ?? getLanguage();
 
-    if (!pluginAttributeMap.containsKey(name)) {
+    if (pluginAttributeMap.containsKey(name)) {
       return null;
     }
 
     final pluginClass = pluginAttributeMap[name];
 
-    if (pluginClass!.isExtension) {
+    if (pluginClass != null && pluginClass.isExtension) {
       if (plugins.containsKey(Tuple2(name, null))) {
         return plugins[Tuple2(name, null)];
       } else {
-        if (check) return null;
-        return initPlugin(attribute: name, language: languageTemp);
+        return check ? null : initPlugin(name, language: language);
       }
     } else {
-      if (plugins.containsKey(Tuple2(name, languageTemp))) {
-        return plugins[Tuple2(name, languageTemp)];
+      if (plugins.containsKey(Tuple2(name, lang))) {
+        return plugins[Tuple2(name, lang)];
       } else {
-        if (check) return null;
-        return initPlugin(attribute: name, language: languageTemp);
+        return check ? null : initPlugin(name, language: language);
       }
     }
   }
 
-  XMLBase? initPlugin({
-    String? attribute,
+  XMLBase? initPlugin(
+    String attribute, {
     String? language,
-    xml.XmlElement? existingXML,
+    xml.XmlElement? existingXml,
     bool reuse = true,
     XMLBase? element,
   }) {
-    String? languageTemp;
-    final defaultLanguage = getLanguage();
-    if (language == null) {
-      languageTemp = defaultLanguage;
-    }
+    final defaultLanguage = language ?? getLanguage();
 
     final pluginClass = pluginAttributeMap[attribute];
 
@@ -294,46 +251,172 @@ class XMLBase {
         plugins.containsKey(Tuple2(attribute, null))) {
       return plugins[Tuple2(attribute, null)];
     }
-    if (reuse && plugins.containsKey(Tuple2(attribute, languageTemp))) {
-      return plugins[Tuple2(attribute, languageTemp)];
+    if (reuse && plugins.containsKey(tuple2(attribute, language))) {
+      return plugins[Tuple2(attribute, language)];
     }
 
-    XMLBase? plugin;
+    late XMLBase? plugin;
 
     if (element != null) {
       plugin = element;
-    } else {}
+    } else {
+      plugin = XMLBase(element: existingXml, parent: WeakReference(this));
+    }
 
-    if (plugin!.isExtension) {
+    if (plugin.isExtension) {
       plugins[Tuple2(attribute, null)] = plugin;
     } else {
-      if (languageTemp != defaultLanguage) {
-        // plugin['lang'] = lang;
+      if (language != defaultLanguage) {
+        plugin['lang'] = language;
       }
-      plugins[Tuple2(attribute, languageTemp)] = plugin;
+      plugins[Tuple2(attribute, language)] = plugin;
     }
 
-    if (pluginIterables.contains(pluginClass)) {
+    if (pluginIterables.contains(plugin)) {
       iterables.add(plugin);
-      if (pluginMultiAttribute.isNotEmpty) {
-        initPlugin(attribute: pluginMultiAttribute);
+      if (plugin.pluginAttributeMap.isNotEmpty) {
+        initPlugin(pluginClass!.pluginMultiAttribute);
       }
     }
 
-    loadedPlugins.add(attribute!);
+    loadedPlugins.add(attribute);
 
     return plugin;
   }
 
-  String? getLanguage([String? language]) {
-    final result = element!.getAttribute('${Echotils.getNamespace('XML')}lang');
-    if (result == null && element != null) {
-      final parent = this.parent;
-      if (parent != null) {
-        /// TODO: cast the language of the parent as String.
+  void appendXML(xml.XmlElement xml) => element!.children.add(xml);
+
+  bool match(Either<String, List<String>> xpath) {
+    Either<String, List<String>> xPath = xpath;
+    xPath = xPath.fold((xpath) => _fixNamespace(xpath), (_) => xpath);
+
+    return xPath.fold<bool>((_) => true, (xpath) {
+      final components = xpath[0].split('@');
+      final tag = components[0];
+      final attributes = components.sublist(1);
+
+      if (tag != name &&
+          tag != '$namespace$name' &&
+          !loadedPlugins.contains(tag) &&
+          pluginAttribute.contains(tag)) return false;
+
+      bool matchedSubstanzas = false;
+
+      for (final substanza in iterables) {
+        if (xpath.sublist(1) == []) break;
+        matchedSubstanzas = substanza.match(right(xpath.sublist(1)));
+
+        if (matchedSubstanzas) break;
       }
+
+      for (final attribute in attributes) {
+        final name = attribute.split('=')[0];
+        final value = attribute.split('=')[1];
+
+        if (name != value) return false;
+      }
+
+      if (xpath.length > 1) {
+        final nextTag = xpath[1];
+        if (subInterfaces.contains(nextTag) && this[nextTag] != null) {
+          return true;
+        }
+
+        if (!matchedSubstanzas && xpath.length > 1) {
+          final nextTag = xpath[1].split('@')[0].split('}')[-1];
+          final languages = <String?>[];
+          for (final entry in plugins.entries) {
+            final name = entry.key;
+            if (name.value1 == nextTag) {
+              languages.add(name.value2);
+            }
+          }
+
+          for (final language in languages) {
+            final plugin = getPlugin(nextTag, language);
+            if (plugin != null && plugin.match(right(xpath.sublist(1)))) {
+              return true;
+            }
+          }
+
+          return false;
+        }
+      }
+
+      return true;
+    });
+  }
+
+  Either<String, List<String>> _fixNamespace(
+    String xpath, [
+    bool split = false,
+    bool propogate = false,
+  ]) =>
+      fixNamespace(xpath, split: split, propogate: propogate);
+
+  dynamic get(String key, [dynamic def]) {
+    final value = this[key];
+    if (value == null || value == '') {
+      return def;
+    }
+    return value;
+  }
+
+  void clear() {
+    for (final child in element!.childElements) {
+      child.childElements.toList().remove(child);
     }
 
+    for (final plugin in plugins.keys) {
+      plugins.remove(plugin);
+    }
+  }
+
+  String? getLanguage() {
+    final result = element!.getAttribute('${Echotils.getNamespace('XML')}lang');
+    if (result == null && parent != null) {
+      return parent['lang'];
+    }
     return result;
+  }
+
+  void setLanguage([String? language]) {
+    deleteLanguage();
+    final attribute = '${Echotils.getNamespace('XML')}lang';
+    if (language != null) {
+      element!.setAttribute(attribute, language);
+    }
+  }
+
+  void deleteLanguage() {
+    final attribute = '${Echotils.getNamespace('XML')}lang';
+    if (element!.getAttribute(attribute) != null) {
+      return element!.removeAttribute(attribute);
+    }
+  }
+
+  dynamic operator [](String fullAttribute) {
+    return fullAttribute;
+  }
+
+  void operator []=(int index, String value) {
+    
+  }
+
+  String get tagname => '$namespace$name';
+
+  String get keys {
+    final buffer = StringBuffer();
+    for (final x in interfaces) {
+      buffer.write(x);
+    }
+    for (final x in loadedPlugins) {
+      buffer.write(x);
+    }
+    buffer.write('lang');
+    if (iterables.isNotEmpty) {
+      buffer.write('substanzas');
+    }
+    return buffer.toString();
   }
 }
