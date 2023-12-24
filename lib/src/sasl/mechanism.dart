51f780eb49aa770e1d2bb1c7dd446cd0e07a6e60
vsevex <vsevex@gmail.com>
2023-12-24 15:59:34 -0800

    create `_Mechanism` base class

---
+++ lib/src/sasl/mechanism.dart
@@ -0,0 +1,45 @@
+part of '../plugins/mechanisms/feature.dart';
+
+abstract class _Mechanism {
+  _Mechanism(
+    this._client, {
+    required this.name,
+    int? priority,
+    Set<String>? requiredCredentials,
+    Set<String>? optionalCredentials,
+    Set<String>? securityOptions,
+  }) {
+    if (priority == null) {
+      this.priority = math.Random().nextInt(100) + 100;
+    } else {
+      this.priority = priority;
+    }
+
+    _requiredCredentials = requiredCredentials ?? <String>{};
+    _optionalCredentials = optionalCredentials ?? <String>{};
+    _securityOptions = securityOptions ?? <String>{};
+  }
+
+  final Whixp _client;
+
+  /// A [String] representing the name of the SASL mechanism.
+  final String name;
+
+  /// An [int] representing the priorityof the SASL mechanism.
+  late final int? priority;
+
+  late final Set<String> _requiredCredentials;
+  late final Set<String> _optionalCredentials;
+  late final Set<String> _securityOptions;
+
+  void _setup(Map<String, String> credentials, [Set<String>? securityOptions]) {
+    if (securityOptions != null) {
+      _securityOptions.addAll(securityOptions);
+    }
+    _client.credentials = credentials;
+  }
+
+  String process();
+
+  String challenge(String challenge);
+}
