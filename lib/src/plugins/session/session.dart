21383efcb5b38dc9f7030259a73424b1fe31ad57
vsevex <vsevex@gmail.com>
2023-12-24 15:57:10 -0800

    feature `session`

---
+++ lib/src/plugins/session/session.dart
@@ -0,0 +1,41 @@
+import 'package:echox/src/plugins/base.dart';
+import 'package:echox/src/plugins/session/stanza.dart';
+import 'package:echox/src/stanza/iq.dart';
+import 'package:echox/src/stream/base.dart';
+
+class FeatureSession extends PluginBase {
+  FeatureSession(this._features, {required super.base})
+      : super('session', description: 'Start Session');
+
+  final StanzaBase _features;
+  late final IQ _iq;
+
+  @override
+  void initialize() {
+    final session = Session();
+    _iq = IQ(transport: base.transport);
+
+    base.registerFeature('session', _handleSessionStart);
+
+    registerStanzaPlugin(_iq, session);
+    registerStanzaPlugin(_features, session);
+  }
+
+  Future<void> _handleSessionStart(StanzaBase features) async {
+    if ((features['session'] as XMLBase)['optional'] != null) {
+      base.transport.sessionStarted = true;
+      return;
+    }
+
+    _iq['type'] = 'set';
+    _iq.enable('session');
+    await _iq.sendIQ(callback: _onStartSession);
+  }
+
+  void _onStartSession(StanzaBase response) {
+    print('session start response: $response');
+    base.features.add('session');
+
+    base.transport.sessionStarted = true;
+  }
+}
---
+++ lib/src/plugins/session/stanza.dart
@@ -0,0 +1,49 @@
+import 'package:echox/src/echotils/echotils.dart';
+import 'package:echox/src/stream/base.dart';
+
+import 'package:xml/xml.dart' as xml;
+
+class Session extends XMLBase {
+  Session()
+      : super(
+          name: 'session',
+          namespace: Echotils.getNamespace('SESSION'),
+          interfaces: {'optional'},
+          pluginAttribute: 'session',
+        ) {
+    addGetters(
+      <Symbol, dynamic Function(dynamic args, XMLBase base)>{
+        const Symbol('optional'): (args, base) {
+          if (base.element!.getAttribute('xmlns') == namespace) {
+            return base.element!.getAttribute('optional') != null;
+          }
+          return false;
+        },
+      },
+    );
+
+    addSetters(
+      <Symbol, void Function(dynamic value, dynamic args, XMLBase base)>{
+        const Symbol('optional'): (value, args, base) {
+          if (value != null) {
+            final optional = xml.XmlElement(xml.XmlName('optional'));
+            base.element!.children.add(optional);
+          } else {
+            delete('optional');
+          }
+        },
+      },
+    );
+
+    addDeleters(
+      <Symbol, dynamic Function(dynamic args, XMLBase base)>{
+        const Symbol('optional'): (args, base) {
+          if (base.element!.getAttribute('xmlns') == namespace) {
+            final optional = base.element!.getElement('optional');
+            base.element!.children.remove(optional);
+          }
+        },
+      },
+    );
+  }
+}
