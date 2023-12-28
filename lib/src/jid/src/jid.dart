00a619805822e3c733ed72e6ad76edb503165a90
vsevex <vsevex@gmail.com>
2023-12-28 11:31:42 -0800

    change from `JabberIDTemp` to `JabberID`

--- lib/src/jid/src/jid.dart
+++ lib/src/jid/src/jid.dart
@@ -164,8 +164,8 @@ class _Jabbered {
   }
 }
 
-class JabberIDTemp {
-  JabberIDTemp([String? jid]) {
+class JabberID {
+  JabberID([String? jid]) {
     if (jid == null) return;
     final jabberID = _Jabbered._parse(jid);
     _node = jabberID.value1;
@@ -248,7 +248,7 @@ class JabberIDTemp {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is JabberIDTemp &&
+      other is JabberID &&
           runtimeType == other.runtimeType &&
           node == other.node &&
           domain == other.domain &&
@@ -266,150 +266,4 @@ class JabberIDTemp {
 
   @override
   String toString() => _full;
-}
-
-/// Represents a Jabber ID, which consists of a local part, a domain part, and
-/// an optional resource part. Provides methods to manipulate Jabber IDs and
-/// manage their components.
-class JabberID {
-  /// Creates a new instance of the [JabberID] class.
-  JabberID(this.local, {this.domain, this.resource}) {
-    setDomain(domain);
-    setLocal(local);
-  }
-
-  /// The local part of the Jabber ID. This is the part that comes before the
-  /// `@` symbol.
-  String local;
-
-  /// The domain part of the Jabber ID. This is the part that comes after the
-  /// local part and `@` sign.
-  String? domain;
-
-  /// The resource part of the Jabber ID. This is the part that comes after the
-  /// domain name.
-  String? resource;
-
-  /// Creates a new instance of the [JabberID] class from a string
-  /// representation.
-  ///
-  /// The [jid] parameter is a string in the format "local@domain/resource". If
-  /// no resource part is provided, the format is "local@domain".
-  ///
-  /// ### Example:
-  /// ```dart
-  /// final jid = JabberID.fromString('local@domain/mobile');
-  ///
-  /// log(jid.local); /// outputs "local"
-  /// ```
-  factory JabberID.fromString(String jid) {
-    final parts = jid.split('/');
-
-    /// The local part of the Jabber ID. This is the part that comes before the
-    /// `@` symbol.
-    late String local;
-
-    /// The domain part of the Jabber ID. This is the part that comes after the
-    ///  local part and `@` sign.
-    String domain;
-
-    /// The resource part of the Jabber ID. This is the part that comes after the
-    /// domain name.
-    String? resource;
-
-    if (!jid.contains('@')) {
-      return JabberID(jid);
-    }
-
-    if (parts.length > 1) {
-      local = parts[0].split('@').first;
-      domain = parts[0].split('@')[1];
-      resource = parts[1];
-    } else {
-      final parts = jid.split('@');
-      local = parts.first;
-      domain = parts[1];
-    }
-
-    return JabberID(local, domain: domain, resource: resource);
-  }
-
-  /// Returns the bare Jabber ID, removing the resource part if present.
-  ///
-  /// ### Example:
-  /// ```dart
-  /// final jid = JabberID('local', domain: 'domain', resource: 'mobile');
-  /// log(jid.bare); /// outputs bare JID as "local@domain"
-  /// ```
-  JabberID get bare {
-    if (resource != null) {
-      return JabberID(local, domain: domain);
-    }
-
-    return this;
-  }
-
-  /// Sets the local part of the Jabber ID. Escapes the local part if necessary.
-  ///
-  /// ### Example:
-  /// ```dart
-  /// final jid = JabberID('local', domain: 'domain');
-  /// jid.setLocal('hert');
-  /// log(jid.local); /// outputs "hert"
-  /// ```
-  void setLocal(String local) {
-    final escaped = _detect(local);
-
-    if (escaped) {
-      this.local = Escaper().escape(local);
-    } else {
-      this.local = local.toLowerCase();
-    }
-  }
-
-  /// Returns the local part of the Jabber ID.
-  ///
-  /// The [unescape] parameter determines whether to unescape the local part.
-  String getLocal({bool unescape = false}) {
-    final local = unescape ? Escaper().unescape(this.local) : this.local;
-
-    return local;
-  }
-
-  /// Sets the domain part of the Jabber ID.
-  void setDomain(String? domain) => this.domain = domain?.toLowerCase();
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is JabberID &&
-          runtimeType == other.runtimeType &&
-          local == other.local &&
-          domain == other.domain &&
-          resource == other.resource;
-
-  @override
-  int get hashCode => local.hashCode ^ domain.hashCode ^ resource.hashCode;
-
-  @override
-  String toString({bool unescape = false}) {
-    late final string = StringBuffer();
-
-    if (domain == null) {
-      return local;
-    }
-
-    string
-      ..write(getLocal(unescape: unescape))
-      ..write('@')
-      ..write(domain);
-
-    if (resource != null) {
-      string
-        ..write('/')
-        ..write(resource);
-    }
-
-    return string.toString();
-  }
 }
