2f20bab7612a6f18da589dbca43988654b647080
vsevex <vsevex@gmail.com>
2023-12-28 11:33:47 -0800

    update test cases according the new `Eventius`

--- test/eventius_test.dart
+++ test/eventius_test.dart
@@ -1,22 +1,19 @@
 import 'package:echox/src/handler/eventius.dart';
 
-import 'package:events_emitter/events_emitter.dart';
-
 import 'package:test/test.dart';
 
 void main() {
   late Eventius eventius;
 
-  setUp(() => eventius = Eventius(EventEmitter()));
+  setUp(() => eventius = Eventius());
 
   group('Eventius class test cases', () {
     test('event happening must work properly', () {
-      final happened = <bool>[];
+      final happened = <bool?>[];
 
-      void testEvent(bool? data) => happened.add(data!);
-      final listener = EventListener<bool?>('test', testEvent);
+      void testEvent(bool? data) => happened.add(data);
 
-      eventius.addEvent(listener);
+      eventius.on<bool>('test', (data) => testEvent(data));
       eventius.emit<bool>('test', true);
       eventius.emit<bool>('test', true);
 
@@ -24,14 +21,14 @@ void main() {
       expect(happened, equals([true, true]));
     });
 
-    test('must create event listener properly', () {
+    test('must create event handler properly', () {
       late String? substring;
-      final listener = eventius.createListener<String>(
-        'test',
-        (data) => substring = data?.substring(1),
-      );
+      void handler(String? data) {
+        substring = data?.substring(1);
+        return;
+      }
 
-      eventius.addEvent(listener);
+      eventius.on<String>('test', handler);
       eventius.emit<String>('test');
       expect(substring, isNull);
       eventius.emit<String>('test', 'blya');
@@ -41,15 +38,12 @@ void main() {
     test('adding multiple listeners should be allowed', () async {
       final happened = <bool>[];
 
-      void testEvent(_) => happened.add(true);
-      void testEventSecond(_) => happened.add(false);
+      void testEvent(bool? data) => happened.add(true);
 
-      final listener = EventListener<bool?>('test', testEvent);
-      final listener2 = EventListener<bool?>('test', testEventSecond);
+      void testEventSecond(bool? data) => happened.add(false);
 
-      eventius
-        ..addEvent(listener)
-        ..addEvent(listener2);
+      eventius.on<bool>('test', testEvent);
+      eventius.on<bool>('test', testEventSecond);
 
       eventius.emit<bool>('test');
       eventius.emit<bool>('test');
@@ -60,8 +54,8 @@ void main() {
 
     test('must properly handle future test cases', () {
       final handlers = <String>[];
-      final listenerAsync =
-          eventius.createListener<String>('testAsync', (data) async {
+
+      eventius.on<String>('testAsync', (data) async {
         if (data != null) {
           Future.delayed(const Duration(milliseconds: 1000), () {
             handlers.add('handled future: $data');
@@ -73,7 +67,7 @@ void main() {
         }
       });
 
-      final listenerSync = eventius.createListener<String>('testSync', (data) {
+      eventius.on<String>('testSync', (data) {
         if (data != null) {
           handlers.add('handled non-future: $data');
         } else {
@@ -81,14 +75,12 @@ void main() {
         }
       });
 
-      eventius.addEvent(listenerAsync);
-      eventius.addEvent(listenerSync);
-      eventius.emit('testAsync', 'hert');
+      eventius.emit<String>('testAsync', 'hert');
       expect(handlers, isEmpty);
-      eventius.emit('testSync', 'hert');
+      eventius.emit<String>('testSync', 'hert');
       expect(handlers.length, equals(1));
       expect(handlers, equals(['handled non-future: hert']));
-      eventius.emit('testAsync', 'cart');
+      eventius.emit<String>('testAsync', 'cart');
       Future.delayed(
         const Duration(milliseconds: 1000),
         () async {
@@ -106,16 +98,15 @@ void main() {
     });
 
     test('test adding, removing, then adding an event handler', () {
-      final happened = <bool>[];
+      final happened = <bool?>[];
 
-      void testEvent(bool? data) => happened.add(data!);
-      final listener = EventListener<bool?>('test', testEvent);
+      void testEvent(bool? data) => happened.add(data);
+      final removeListener = eventius.on<bool>('test', testEvent);
 
-      eventius.addEvent(listener);
       eventius.emit<bool>('test', true);
-      eventius.removeEventHandler<bool>(listener: listener);
+      removeListener.call();
       eventius.emit<bool>('test', true);
-      eventius.addEvent(listener);
+      eventius.on<bool>('test', testEvent);
       eventius.emit<bool>('test', true);
 
       expect(happened.length, equals(2));
@@ -123,32 +114,30 @@ void main() {
     });
 
     test('try throwing Exception while calling the handler', () {
-      void testEvent(data) {
+      void testEvent(String? data) {
         if (data != null) return;
         throw Exception('some error');
       }
 
-      final listener = eventius.createListener('test', testEvent);
+      eventius.on('test', testEvent);
 
-      eventius.addEvent(listener);
       expect(
-        () => eventius.emit('test', null),
+        () => eventius.emit<String>('test'),
         throwsA(const TypeMatcher<Exception>()),
       );
-      expect(() => eventius.emit('test', 'cart'), returnsNormally);
+      expect(() => eventius.emit<String>('test', 'cart'), returnsNormally);
     });
 
     test(
         'must call callback once, then dispose the given callback and do not run on call',
         () {
-      final happened = <bool>[];
+      final happened = <bool?>[];
 
-      void testEvent(bool? data) => happened.add(data!);
-      final listener = EventListener<bool?>('test', testEvent, once: true);
+      void testEvent(bool? data) => happened.add(data);
+      eventius.once<bool>('test', testEvent);
 
-      eventius.addEvent(listener);
-      eventius.emit('test', true);
-      eventius.emit('test', true);
+      eventius.emit<bool>('test', true);
+      eventius.emit<bool>('test', true);
 
       expect(happened.length, equals(1));
       expect(happened, equals([true]));
