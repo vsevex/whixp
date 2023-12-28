90483ba015759b0ddb52534611faccaaf36840aa
vsevex <vsevex@gmail.com>
2023-12-28 11:30:14 -0800

    refactor(echox): remove unused files

--- lib/src/error/error.dart
+++
@@ -1,7 +0,0 @@
-/// Provides methods and properties to extract error information from XML
-/// elements and create instance of errors.
-///
-/// {@category Core}
-library;
-
-export 'src/mishap.dart';
