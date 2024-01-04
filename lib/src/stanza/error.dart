8b8bb2313b94a12a9d73204487455be69e22dfc8
vsevex <vsevex@gmail.com>
2024-01-04 04:01:30 +0400

    add StreamError class

--- lib/src/stanza/error.dart
+++ lib/src/stanza/error.dart
@@ -34,7 +34,9 @@ class StanzaError extends XMLBase implements Exception {
           for (final child in base.element!.childElements) {
             if (child.getAttribute('xmlns') == _conditionNamespace) {
               final condition = child.localName;
-              return condition;
+              if (_conditions.contains(condition)) {
+                return condition;
+              }
             }
           }
           return '';
@@ -45,33 +47,86 @@ class StanzaError extends XMLBase implements Exception {
     );
   }
 
-  final conditions = {
-    'bad-format',
-    'bad-namespace-prefix',
-    'conflict',
-    'connection-timeout',
-    'host-gone',
-    'host-unknown',
-    'improper-addressing',
-    'internal-server-error',
-    'invalid-from',
-    'invalid-namespace',
-    'invalid-xml',
-    'not-authorized',
-    'not-well-formed',
-    'policy-violation',
-    'remote-connection-failed',
-    'reset',
-    'resource-constraint',
-    'restricted-xml',
-    'see-other-host',
-    'system-shutdown',
-    'undefined-condition',
-    'unsupported-encoding',
-    'unsupported-feature',
-    'unsupported-stanza-type',
-    'unsupported-version',
-  };
+  late final String _conditionNamespace;
+}
+
+class StreamError extends StanzaBase implements Exception {
+  StreamError({String? conditionNamespace, super.includeNamespace = false})
+      : super(
+          name: 'error',
+          namespace: Echotils.getNamespace('JABBER_STREAM'),
+          pluginAttribute: 'error',
+          interfaces: {'condition', 'text', 'see_other_host'},
+        ) {
+    _conditionNamespace = conditionNamespace ?? Echotils.getNamespace('STREAM');
+
+    addGetters(
+      <Symbol, dynamic Function(dynamic args, XMLBase base)>{
+        const Symbol('see_other_host'): (args, base) {
+          final namespace = _conditionNamespace;
+
+          return base.getSubText(
+            "//*[local-name()='see-other-host' and namespace-uri()='$namespace']",
+          );
+        },
+      },
+    );
+
+    addSetters(
+      <Symbol, dynamic Function(dynamic value, dynamic args, XMLBase base)>{
+        const Symbol('see_other_host'): (value, args, base) {
+          if (value is String && value.isNotEmpty) {
+            base.delete('condition');
+
+            final namespace = _conditionNamespace;
+            base.setSubText(
+              "//*[local-name()='see-other-host' and namespace-uri()='$namespace']",
+            );
+          }
+        },
+      },
+    );
+
+    addDeleters(
+      <Symbol, dynamic Function(dynamic args, XMLBase base)>{
+        const Symbol('see_other_host'): (args, base) {
+          final namespace = _conditionNamespace;
+
+          return base.deleteSub(
+            "//*[local-name()='see-other-host' and namespace-uri()='$namespace']",
+          );
+        },
+      },
+    );
+  }
 
   late final String _conditionNamespace;
 }
+
+const _conditions = {
+  'bad-format',
+  'bad-namespace-prefix',
+  'conflict',
+  'connection-timeout',
+  'host-gone',
+  'host-unknown',
+  'improper-addressing',
+  'internal-server-error',
+  'invalid-from',
+  'invalid-namespace',
+  'invalid-xml',
+  'not-authorized',
+  'not-well-formed',
+  'policy-violation',
+  'remote-connection-failed',
+  'reset',
+  'resource-constraint',
+  'restricted-xml',
+  'see-other-host',
+  'system-shutdown',
+  'undefined-condition',
+  'unsupported-encoding',
+  'unsupported-feature',
+  'unsupported-stanza-type',
+  'unsupported-version',
+};
