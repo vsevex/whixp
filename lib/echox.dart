3c083bb4cda28e664db5c08d541e299a005f8f73
vsevex <vsevex@gmail.com>
2024-01-07 03:25:23 +0400

    remove unused project files

--- lib/echox.dart
+++
@@ -1,8 +0,0 @@
-/// Serves as the foundation for establishing and managing XMPP (Extensible
-/// Messaging and Presence Protocol) connections.
-library echox;
-
-export 'src/echotils/echotils.dart';
-export 'src/exception.dart';
-export 'src/jid/jid.dart';
-export 'src/log/log.dart';
--- lib/src/echotils/echotils.dart
+++
@@ -1,6 +0,0 @@
-/// A utility class for various EchoX-related operations.
-///
-/// {@category Utilities}
-library;
-
-export 'src/echotils.dart';
