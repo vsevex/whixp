89cb94469cac5fb40139d5bd2325ec7a8004f40c
vsevex <vsevex@gmail.com>
2024-01-15 02:49:14 +0400

    export dataforms plugin to public (hide field, option, form)

--- lib/src/plugins/plugins.dart
+++ lib/src/plugins/plugins.dart
@@ -1,3 +1,4 @@
 export 'disco/disco.dart'
     hide DiscoInformationAbstract, DiscoItem, DiscoItemsAbstract;
+export 'form/dataforms.dart' hide FieldOption, FormAbstract, FormFieldAbstract;
 export 'ping/ping.dart' hide PingStanza;
