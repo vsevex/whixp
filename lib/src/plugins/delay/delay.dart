6f9211eb45cc59b10dfb392021cb54f1e1b3f2c5
vsevex <vsevex@gmail.com>
2024-01-22 01:00:42 +0400

    feat(delay): update plugin and its stanza

--- lib/src/plugins/delay/delay.dart
+++ lib/src/plugins/delay/delay.dart
@@ -1,6 +1,7 @@
-import 'package:meta/meta.dart';
-
+import 'package:whixp/src/jid/jid.dart';
+import 'package:whixp/src/log/log.dart';
 import 'package:whixp/src/plugins/base.dart';
+import 'package:whixp/src/plugins/time/time.dart';
 import 'package:whixp/src/stream/base.dart';
 
 import 'package:xml/xml.dart' as xml;
@@ -34,16 +35,85 @@ part 'stanza.dart';
 ///
 /// see <https://xmpp.org/extensions/xep-0203.html>
 class Delay extends PluginBase {
+  /// XMPP stanzas are sometimes withheld for delivery due to the receipent
+  /// being offline, or are resent in order to establish recent history as is
+  /// the case with MUCs. In any case, it is impoprtant to konw when the stanza
+  /// was originally sent, not just when it was last received.
+  ///
+  /// ### Example:
+  /// ```dart
+  /// void main() {
+  ///   final delay = Delay();
+  ///   /// ...register this plugin in the Whixp instance.
+  ///
+  ///   addEventHandler<Message>('someEvent', (message) {
+  ///     final delayedMessage = (message['delay'] as DelayStanza);
+  ///     /// ...do whatever you need on this stanza.
+  ///   });
+  /// }
+  /// ```
+  ///
+  /// The initialization of this plugin is not mandatory due the corresponding
+  /// stanza for this plugin will be registered beforehand. So, you will only
+  /// need to capture the incoming Message or Presence stanzas and parse delayed
+  /// message by type casting.
   Delay() : super('delay', description: 'XEP-0203: Delayed Delivery');
 
-  @override
-  void pluginInitialize() {}
+  late final DateTimeProfile? _time;
 
-  /// Do not import.
+  /// Gets timestamp from the provided [DelayStanza].
+  DateTime? getStamp(DelayStanza stanza) {
+    if (_time == null) {
+      Log.instance.warning(
+        'In order to parse timestamp, you need to register XMPP Date and Time Profiles plugin',
+      );
+      return null;
+    }
+
+    final timestamp = stanza.getAttribute('stamp');
+    if (timestamp.isEmpty) {
+      return null;
+    }
+    return _time.parse(timestamp);
+  }
+
+  /// Sets the provided [value] to the stanza as timestamp.
+  ///
+  /// Provided [value] must be [DateTime] or [String].
+  void setStamp(DelayStanza stanza, dynamic value) {
+    assert(
+      value is DateTime || value is String,
+      'Provided value must be either DateTime or String',
+    );
+
+    if (_time == null) {
+      Log.instance.warning(
+        'In order to parse timestamp, you need to register XMPP Date and Time Profiles plugin',
+      );
+      return;
+    }
+
+    late String timestamp;
+
+    if (value is DateTime) {
+      timestamp = _time.format(value.toUtc());
+    } else {
+      timestamp = value as String;
+    }
+
+    stanza.setAttribute('stamp', timestamp);
+  }
+
+  @override
+  void pluginInitialize() {
+    _time = base.getPluginInstance<DateTimeProfile>('time');
+  }
+
+  /// Do not implement.
   @override
   void sessionBind(String? jid) {}
 
-  /// Do not import.
+  /// Do not implement.
   @override
   void pluginEnd() {}
 }
--- lib/src/plugins/delay/stanza.dart
+++ lib/src/plugins/delay/stanza.dart
@@ -1,17 +1,51 @@
 part of 'delay.dart';
 
-@internal
 class DelayStanza extends XMLBase {
-  DelayStanza({super.element, super.parent})
-      : super(
+  DelayStanza({
+    super.pluginTagMapping,
+    super.pluginAttributeMapping,
+    super.getters,
+    super.setters,
+    super.element,
+    super.parent,
+  }) : super(
           name: 'delay',
           namespace: 'urn:xmpp:delay',
           pluginAttribute: 'delay',
           interfaces: {'from', 'stamp', 'text'},
-        );
+        ) {
+    addGetters(<Symbol, dynamic Function(dynamic args, XMLBase base)>{
+      const Symbol('from'): (args, base) => from,
+      const Symbol('text'): (args, base) => text,
+    });
+
+    addSetters(<Symbol,
+        dynamic Function(dynamic value, dynamic args, XMLBase base)>{
+      const Symbol('from'): (value, args, base) => setFrom(value as JabberID),
+      const Symbol('text'): (value, args, base) => text = value as String,
+    });
+  }
+
+  JabberID? get from {
+    final jid = getAttribute('from');
+    if (jid.isNotEmpty) {
+      return JabberID(jid);
+    }
+    return null;
+  }
+
+  void setFrom(JabberID jid) => setAttribute('from', jid.toString());
+
+  String get text => element!.innerText;
+
+  set text(String value) => element!.innerText = value;
 
   @override
   DelayStanza copy({xml.XmlElement? element, XMLBase? parent}) => DelayStanza(
+        pluginTagMapping: pluginTagMapping,
+        pluginAttributeMapping: pluginAttributeMapping,
+        getters: getters,
+        setters: setters,
         element: element,
         parent: parent,
       );
