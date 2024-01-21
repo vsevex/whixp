98e1ee35bc7c0fb9b93e45ac838b63a4262a2227
vsevex <vsevex@gmail.com>
2024-01-22 01:06:03 +0400

    fix(rsm): parsing and iterating is now working

--- lib/src/plugins/rsm/rsm.dart
+++ lib/src/plugins/rsm/rsm.dart
@@ -1,50 +1,69 @@
+import 'dart:async';
+
 import 'package:whixp/src/plugins/base.dart';
 import 'package:whixp/src/plugins/disco/disco.dart';
-import 'package:whixp/src/plugins/rsm/stanza.dart';
+import 'package:whixp/src/stanza/error.dart';
 import 'package:whixp/src/stanza/iq.dart';
 import 'package:whixp/src/stream/base.dart';
 import 'package:whixp/src/utils/utils.dart';
 
+import 'package:xml/xml.dart' as xml;
+
+part 'stanza.dart';
+
 /// Result Set Management plugin
 class RSM extends PluginBase {
-  RSM(this._disco)
+  RSM()
       : super(
           'RSM',
           description: 'XEP-0059: Result Set Management',
           dependencies: {'disco'},
         );
 
-  final ServiceDiscovery _disco;
+  late final ServiceDiscovery? _disco;
 
+  /// Do not implement.
   @override
   void pluginInitialize() {}
 
   @override
-  void sessionBind(String? jid) => _disco.addFeature(RSMStanza().namespace);
+  void sessionBind(String? jid) {
+    _disco = base.getPluginInstance<ServiceDiscovery>('disco');
+    if (_disco != null) {
+      _disco.addFeature(RSMStanza().namespace);
+    }
+  }
 
   @override
-  void pluginEnd() => _disco.removeFeature(RSMStanza().namespace);
+  void pluginEnd() => _disco?.removeFeature(RSMStanza().namespace);
 
-  _ResultIterator iterate(
+  _ResultIterable iterate(
     IQ stanza,
     String interface, {
-    String results = 'substanzas',
     int amount = 10,
     bool reverse = false,
     String receiveInterface = '',
-    T Function<T>(IQ stanza)? preCallback,
     XMLBase? receiveInterfaceStanza,
-    B Function<B>(StanzaBase stanza)? postCallback,
+    T Function<T>(IQ iq)? preCallback,
+    FutureOr<void> Function(IQ iq)? postCallback,
+    FutureOr<void> Function(StanzaError error)? failureCallback,
+    FutureOr<void> Function()? timeoutCallback,
+    int timeout = 10,
   }) =>
-      _ResultIterator(
-        stanza,
-        interface,
-        amount: amount,
-        reverse: reverse,
-        receiveInterface: receiveInterface,
-        preCallback: preCallback,
-        receiveInterfaceStanza: receiveInterfaceStanza,
-        postCallback: postCallback,
+      _ResultIterable(
+        _ResultIterator(
+          stanza,
+          interface,
+          amount: amount,
+          reverse: reverse,
+          receiveInterface:
+              receiveInterface.isEmpty ? interface : receiveInterface,
+          preCallback: preCallback,
+          postCallback: postCallback,
+          failureCallback: failureCallback,
+          timeoutCallback: timeoutCallback,
+          timeout: timeout,
+        ),
       );
 }
 
@@ -53,53 +72,56 @@ class _ResultIterator {
   _ResultIterator(
     this._query,
     this._interface, {
-    String receiveInterface = '',
-    int amount = 10,
-    bool reverse = false,
-    T Function<T>(IQ stanza)? preCallback,
-    XMLBase? receiveInterfaceStanza,
-    B Function<B>(StanzaBase stanza)? postCallback,
+    required int amount,
+    required bool reverse,
+    required String receiveInterface,
+    T Function<T>(IQ iq)? preCallback,
+    FutureOr<void> Function(IQ iq)? postCallback,
+    FutureOr<void> Function(StanzaError error)? failureCallback,
+    FutureOr<void> Function()? timeoutCallback,
+    int timeout = 10,
   }) {
-    _receiveInterface = receiveInterface;
     _amount = amount;
     _reverse = reverse;
+    _receiveInterface = receiveInterface;
     _preCallback = preCallback;
-    _receiveInterfaceStanza = receiveInterfaceStanza;
     _postCallback = postCallback;
-
-    iterator = _ResultIterable(this);
+    _failureCallback = failureCallback;
+    _timeoutCallback = timeoutCallback;
+    _timeout = timeout;
   }
 
   bool _stop = false;
-  late final bool _reverse;
+  String? start;
+
   final IQ _query;
   final String _interface;
-  late final String _receiveInterface;
-  String? start;
-  late final T Function<T>(IQ stanza)? _preCallback;
-  late final B Function<B>(StanzaBase stanza)? _postCallback;
-  late final XMLBase? _receiveInterfaceStanza;
   late final int _amount;
-
-  late final _ResultIterable iterator;
+  late final bool _reverse;
+  late final String _receiveInterface;
+  late final T Function<T>(IQ iq)? _preCallback;
+  late final FutureOr<void> Function(IQ iq)? _postCallback;
+  FutureOr<void> Function(StanzaError error)? _failureCallback;
+  FutureOr<void> Function()? _timeoutCallback;
+  int _timeout = 10;
 }
 
-class _ResultIterable extends Iterable<StanzaBase?> {
+class _ResultIterable extends Iterable<Future<StanzaBase?>> {
   _ResultIterable(this.base);
 
   final _ResultIterator base;
 
   @override
-  Iterator<StanzaBase?> get iterator => _ResultIteratorBase(base);
+  Iterator<Future<StanzaBase?>> get iterator => _ResultIteratorBase(base);
 }
 
-class _ResultIteratorBase implements Iterator<StanzaBase?> {
+class _ResultIteratorBase implements Iterator<Future<StanzaBase?>> {
   _ResultIteratorBase(this.iterator);
 
   final _ResultIterator iterator;
 
   @override
-  StanzaBase? get current {
+  Future<StanzaBase?> get current async {
     iterator._query['id'] = WhixpUtils.getUniqueId('rsm');
     ((iterator._query[iterator._interface] as XMLBase)['rsm']
         as RSMStanza)['max'] = iterator._amount.toString();
@@ -121,50 +143,51 @@ class _ResultIteratorBase implements Iterator<StanzaBase?> {
       if (iterator._preCallback != null) {
         iterator._preCallback?.call(iterator._query);
       }
-      late StanzaBase base;
-      iterator._query.sendIQ(
-        callback: (stanza) {
-          if (iterator._receiveInterfaceStanza != null) {
-            stanza.registerPlugin(iterator._receiveInterfaceStanza!);
-            if (((stanza[iterator._receiveInterface] as XMLBase)['rsm']
-                        as RSMStanza)['first'] !=
-                    null &&
-                ((stanza[iterator._receiveInterface] as XMLBase)['rsm']
-                        as RSMStanza)['last'] !=
-                    null) {
-              return false;
-            }
+      IQ? stanza;
 
-            if (iterator._postCallback != null) {
-              iterator._postCallback?.call(stanza);
-            }
+      await iterator._query.sendIQ(
+        callback: (iq) async {
+          final received = iq[iterator._receiveInterface] as XMLBase;
 
-            if (((stanza[iterator._receiveInterface] as XMLBase)['rsm']
-                        as RSMStanza)['count'] !=
-                    null &&
-                ((stanza[iterator._receiveInterface] as XMLBase)['rsm']
-                        as RSMStanza)['firstIndex'] !=
-                    null) {
-              // final count = int.parse(((stanza[iterator._receiveInterface] as XMLBase)['rsm'] as RSMStanza)['count'] as String);
-              // final first = int.parse(((stanza[iterator._receiveInterface] as XMLBase)['rsm'] as RSMStanza)['firstIndex'] as String);
+          if ((received['rsm'] as RSMStanza)['first'] == null &&
+              (received['rsm'] as RSMStanza)['last'] == null) {
+            throw Exception('Stop stanza iteration: $iq');
+          }
+
+          if (iterator._postCallback != null) {
+            await iterator._postCallback!.call(iq);
+          }
+
+          if ((received['rsm'] as RSMStanza)['count'] != null &&
+              (received['rsm'] as RSMStanza)['first_index'] != null) {
+            final count = int.parse(
+              (received['rsm'] as RSMStanza)['count'] as String,
+            );
+            final first = int.parse((received['rsm'] as RSMStanza).firstIndex);
+
+            final numberItems =
+                (received['substanzas'] as List<XMLBase>).length;
+            if (first + numberItems == count) {
               iterator._stop = true;
             }
-
-            if (iterator._reverse) {
-              iterator.start = ((stanza[iterator._receiveInterface]
-                  as XMLBase)['rsm'] as RSMStanza)['first'] as String;
-            } else {
-              iterator.start = ((stanza[iterator._receiveInterface]
-                  as XMLBase)['rsm'] as RSMStanza)['last'] as String;
-            }
-
-            base = stanza;
           }
+
+          if (iterator._reverse) {
+            iterator.start = (received['rsm'] as RSMStanza)['first'] as String;
+          } else {
+            iterator.start = (received['rsm'] as RSMStanza)['last'] as String;
+          }
+
+          stanza = iq;
         },
+        failureCallback: iterator._failureCallback,
+        timeoutCallback: iterator._timeoutCallback,
+        timeout: iterator._timeout,
       );
 
-      return base;
+      return Future.value(stanza);
     } on Exception {
+      iterator._stop = true;
       return null;
     }
   }
