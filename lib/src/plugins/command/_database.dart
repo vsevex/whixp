6035480f524819f4fc2bb9ffc21c003023f80bdc
vsevex <vsevex@gmail.com>
2024-03-01 17:54:28 +0400

    feat(commands): add hive database support for this plugin

---
+++ lib/src/plugins/command/_database.dart
@@ -0,0 +1,16 @@
+part of 'command.dart';
+
+class _HiveDatabase {
+  factory _HiveDatabase() => _instance;
+
+  _HiveDatabase._();
+
+  late Box<Map<dynamic, dynamic>> box;
+
+  static final _HiveDatabase _instance = _HiveDatabase._();
+
+  Future<void> initialize(String name, [String? path]) async =>
+      box = await Hive.openBox<Map<dynamic, dynamic>>(name, path: path);
+
+  Map<dynamic, dynamic>? getSession(String sessionID) => box.get(sessionID);
+}
