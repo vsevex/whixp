9eb744d6c59daeeee386be0dfcb6d5e7f7712d87
vsevex <vsevex@gmail.com>
2024-08-18 00:08:30 +0400

    refactor: Update XMPP classes for improved code organization and functionality

--- lib/src/stanza/iq.dart
+++ lib/src/stanza/iq.dart
@@ -1,17 +1,14 @@
 import 'dart:async';
 
 import 'package:synchronized/extension.dart';
+
 import 'package:whixp/src/exception.dart';
 import 'package:whixp/src/handler/handler.dart';
-import 'package:whixp/src/jid/jid.dart';
 import 'package:whixp/src/log/log.dart';
-import 'package:whixp/src/plugins/bind/bind.dart';
-import 'package:whixp/src/plugins/plugins.dart';
 import 'package:whixp/src/stanza/error.dart';
-import 'package:whixp/src/stanza/root.dart';
-import 'package:whixp/src/stanza/roster.dart';
-import 'package:whixp/src/stream/base.dart';
-import 'package:whixp/src/stream/matcher/matcher.dart';
+import 'package:whixp/src/stanza/mixins.dart';
+import 'package:whixp/src/stanza/node.dart';
+import 'package:whixp/src/stanza/stanza.dart';
 import 'package:whixp/src/transport.dart';
 import 'package:whixp/src/utils/utils.dart';
 
@@ -41,125 +38,84 @@ import 'package:xml/xml.dart' as xml;
 /// ```
 ///
 /// For more information on "id" and "type" please refer to [XML stanzas](https://xmpp.org/rfcs/rfc3920.html#stanzas)
-class IQ extends RootStanza {
-  /// All parameters are extended from [RootStanza]. For more information please
-  /// take a look at [RootStanza].
-  IQ({
-    bool generateID = true,
-    super.stanzaType,
-    super.stanzaTo,
-    super.stanzaFrom,
-    super.stanzaID,
-    super.transport,
-    super.subInterfaces,
-    super.languageInterfaces,
-    super.includeNamespace = false,
-    super.getters,
-    super.setters,
-    super.deleters,
-    super.pluginTagMapping,
-    super.pluginAttributeMapping,
-    super.pluginMultiAttribute,
-    super.pluginIterables,
-    super.overrides,
-    super.isExtension,
-    super.boolInterfaces,
-    super.receive,
-    super.element,
-    super.parent,
-  }) : super(
-          name: 'iq',
-          namespace: WhixpUtils.getNamespace('CLIENT'),
-          interfaces: {'type', 'to', 'from', 'id', 'query'},
-          types: {'get', 'result', 'set', 'error'},
-          pluginAttribute: 'iq',
-        ) {
-    _generateID = generateID;
-    if (_generateID) {
-      if (!receive && (this['id'] == '' || this['id'] == null)) {
-        if (transport != null) {
-          this['id'] = WhixpUtils.getUniqueId();
-        } else {
-          this['id'] = '0';
-        }
+class IQ extends Stanza with Attributes {
+  static const String _name = 'iq';
+
+  /// Constructs an IQ stanza.
+  IQ({bool generateID = false}) {
+    if (generateID) id = WhixpUtils.generateUniqueID();
+    _handlerID = WhixpUtils.generateUniqueID('handler');
+  }
+
+  /// `Error` stanza associated with this IQ stanza, if any.
+  ErrorStanza? error;
+
+  /// Payload of the IQ stanza.
+  Stanza? payload;
+
+  /// Any other XML node associated with this IQ stanza.
+  Node? any;
+
+  String? _handlerID;
+
+  /// Constructs an IQ stanza from a string representation.
+  ///
+  /// Throws [WhixpInternalException] if the input XML is invalid.
+  factory IQ.fromString(String stanza) {
+    try {
+      final root = xml.XmlDocument.parse(stanza).rootElement;
+
+      return IQ.fromXML(root);
+    } catch (_) {
+      throw WhixpInternalException.invalidXML();
+    }
+  }
+
+  /// Constructs an IQ stanza from an XML element node.
+  ///
+  /// Throws [WhixpInternalException] if the provided XML node is invalid.
+  factory IQ.fromXML(xml.XmlElement node) {
+    if (node.localName != _name) {
+      throw WhixpInternalException.invalidNode(node.localName, _name);
+    }
+
+    final iq = IQ();
+    iq.loadAttributes(node);
+
+    for (final child in node.children.whereType<xml.XmlElement>()) {
+      switch (child.localName) {
+        case 'error':
+          iq.error = ErrorStanza.fromXML(child);
+        default:
+          try {
+            final tag = WhixpUtils.generateNamespacedElement(child);
+
+            iq.payload = Stanza.payloadFromXML(tag, child);
+          } on WhixpException catch (exception) {
+            iq.any = Node.fromXML(child);
+            Log.instance.warning(exception.message);
+          }
       }
     }
 
-    addSetters(
-      <Symbol, void Function(dynamic value, dynamic args, XMLBase base)>{
-        const Symbol('query'): (value, args, base) {
-          xml.XmlElement? query = base.element!.getElement(value as String);
-          if (query == null && value.isNotEmpty) {
-            final plugin = base
-                .pluginTagMapping['<${base.name} xmlns="${base.namespace}"/>'];
-            if (plugin != null) {
-              base.enable(plugin.pluginAttribute);
-            } else {
-              base.clear();
-              query = WhixpUtils.xmlElement('query', namespace: value);
-              base.element!.children.add(query);
-            }
-          }
-        },
-      },
-    );
-
-    addGetters(
-      <Symbol, String? Function(dynamic args, XMLBase base)>{
-        const Symbol('query'): (args, base) {
-          for (final child in base.element!.childElements) {
-            if (child.qualifiedName.endsWith('query')) {
-              final namespace = child.getAttribute('xmlns');
-              return namespace;
-            }
-          }
-          return '';
-        },
-      },
-    );
-
-    addDeleters(
-      <Symbol, void Function(dynamic args, XMLBase base)>{
-        const Symbol('query'): (args, base) {
-          final elements = <xml.XmlElement>[];
-          for (final child in base.element!.childElements) {
-            if (child.qualifiedName.endsWith('query')) {
-              elements.add(child);
-            }
-          }
-
-          for (final element in elements) {
-            base.element!.children.removeWhere((el) => el == element);
-          }
-        },
-      },
-    );
-
-    /// Register all required stanzas beforehand, so we won't need to declare
-    /// them one by one whenever there is a need to specific stanza.
-    ///
-    /// If you have not used the specified stanza, then you have to enable the
-    /// stanza through the usage of `pluginAttribute` parameter.
-    registerPlugin(StanzaError());
-    registerPlugin(BindStanza());
-    registerPlugin(Roster());
-    registerPlugin(Form());
-    registerPlugin(PingStanza());
-    registerPlugin(DiscoveryItems());
-    registerPlugin(DiscoveryInformation());
-    registerPlugin(RSMStanza());
-    registerPlugin(PubSubStanza());
-    registerPlugin(PubSubOwnerStanza());
-    registerPlugin(Register());
-    registerPlugin(VCardTempStanza());
-    registerPlugin(Command());
+    return iq;
   }
 
-  /// The id of the attached [Handler].
-  String? _handlerID;
+  /// Converts the IQ stanza to its XML representation.
+  @override
+  xml.XmlElement toXML() {
+    final dictionary = attributeHash;
+    final builder = WhixpUtils.makeGenerator();
 
-  /// Indicates that whether generate ID or not.
-  late final bool _generateID;
+    builder.element(_name, attributes: dictionary);
+    final root = builder.buildDocument().rootElement;
+
+    if (payload != null) root.children.add(payload!.toXML().copy());
+    if (error != null) root.children.add(error!.toXML().copy());
+    if (any != null) root.children.add(any!.toXML().copy());
+
+    return root;
+  }
 
   /// Sends an IQ stanza over the XML stream.
   ///
@@ -172,7 +128,7 @@ class IQ extends RootStanza {
   ///
   /// You can set the return of the callback return type you have provided or
   /// just avoid this.
-  FutureOr<IQ> sendIQ<T>({
+  FutureOr<IQ> send<T>({
     /// Sync or async callback function which accepts the incoming "result"
     /// iq stanza.
     FutureOr<T> Function(IQ iq)? callback,
@@ -181,7 +137,7 @@ class IQ extends RootStanza {
     ///
     /// It is handy way of handling the failure, 'cause the [Completer] can not
     /// handle the uncaught exceptions.
-    FutureOr<void> Function(StanzaError error)? failureCallback,
+    FutureOr<void> Function(ErrorStanza error)? failureCallback,
 
     /// Whenever there is a timeout, this callback method will be called.
     FutureOr<void> Function()? timeoutCallback,
@@ -189,68 +145,44 @@ class IQ extends RootStanza {
     /// The length of time (in seconds) to wait for a response before the
     /// [timeoutCallback] is called, instead of the sync callback. Defaults to
     /// `10` seconds.
-    int timeout = 10,
+    int timeout = 5,
   }) async {
     final completer = Completer<IQ?>();
     final errorCompleter = Completer<IQ?>();
     final resultCompleter = Completer<IQ>();
 
     Handler? handler;
-    BaseMatcher? matcher;
 
-    if (transport!.sessionBind) {
-      final toJID = to ?? JabberID(transport!.boundJID.domain);
-
-      matcher = MatchIDSender(
-        IDMatcherCriteria(
-          transport!.boundJID,
-          toJID,
-          this['id'] as String,
-        ),
-      );
-    } else {
-      matcher = MatcherID(this['id'] as String);
-    }
-
-    Future<void> successCallback(StanzaBase iq) async {
-      IQ stanza;
-      if (iq is! IQ) {
-        stanza = IQ(element: iq.element);
-      } else {
-        stanza = iq;
-      }
-      final type = stanza['type'];
+    Future<void> successCallback(IQ iq) async {
+      final type = iq.type;
 
       if (type == 'result') {
         if (!completer.isCompleted) {
-          completer.complete(stanza);
-          resultCompleter.complete(stanza);
+          completer.complete(iq);
+          resultCompleter.complete(iq);
           errorCompleter.complete(null);
         }
       } else if (type == 'error') {
         if (!completer.isCompleted && !errorCompleter.isCompleted) {
           try {
             completer.complete(null);
-            errorCompleter.complete(stanza);
-            resultCompleter.complete(stanza);
-
-            throw StanzaException.iq(stanza);
+            errorCompleter.complete(iq);
+            resultCompleter.complete(iq);
           } catch (error) {
             Log.instance.error(error.toString());
           }
         }
       } else {
-        handler = FutureCallbackHandler(
+        handler = Handler(
           _handlerID!,
-          (stanza) => Future.microtask(() => successCallback(stanza))
+          (stanza) => Future.microtask(() => successCallback(stanza as IQ))
               .timeout(Duration(seconds: timeout)),
-          matcher: matcher!,
-        );
+        )..id(id!);
 
-        transport?.registerHandler(handler!);
+        Transport.instance().registerHandler(handler!);
       }
 
-      transport?.cancelSchedule(_handlerID!);
+      Transport.instance().cancelSchedule(_handlerID!);
 
       /// Run provided callback if there is any completed IQ stanza.
       if (callback != null) {
@@ -263,8 +195,8 @@ class IQ extends RootStanza {
       /// Run provided failure callback if there is any completed error stanza.
       if (failureCallback != null) {
         final result = await errorCompleter.future;
-        if (result != null) {
-          await failureCallback.call(result['error'] as StanzaError);
+        if (result != null && result.error != null) {
+          await failureCallback.call(result.error!);
         }
       }
     }
@@ -272,12 +204,10 @@ class IQ extends RootStanza {
     void callbackTimeout() {
       runZonedGuarded(
         () {
-          if (!resultCompleter.isCompleted) {
-            throw StanzaException.timeout(this);
-          }
+          if (!resultCompleter.isCompleted) throw StanzaException.timeout(this);
         },
         (error, trace) {
-          transport?.removeHandler(_handlerID!);
+          Transport.instance().removeHandler(_handlerID!);
           if (timeoutCallback != null) {
             timeoutCallback.call();
           }
@@ -285,75 +215,48 @@ class IQ extends RootStanza {
       );
     }
 
-    if (<String>{'get', 'set'}.contains(this['type'] as String)) {
-      _handlerID = this['id'] as String;
-
-      handler = FutureCallbackHandler(
+    if (<String>{'get', 'set'}.contains(type)) {
+      handler = Handler(
         _handlerID!,
-        (iq) => Future.microtask(() => successCallback(iq))
+        (iq) => Future.microtask(() => successCallback(iq as IQ))
             .timeout(Duration(seconds: timeout)),
-        matcher: matcher,
-      );
+      )..id(id!);
 
-      transport!
-        ..schedule(_handlerID!, callbackTimeout, seconds: timeout)
-        ..registerHandler(handler!);
+      Transport.instance()
+        ..registerHandler(handler!)
+        ..schedule(handler!.name, callbackTimeout, seconds: timeout);
     }
-    send();
+    Transport.instance().send(this);
 
     return synchronized(() => resultCompleter.future);
   }
 
-  /// Send a 'feature-not-implemented' error stanza if the stanza is not
-  /// handled.
-  @override
-  void unhandled([Transport? transport]) {
-    if ({'get', 'set'}.contains(this['type'])) {
-      final iq = replyIQ();
-      iq.transport ??= transport ?? this.transport;
-      iq
-        ..registerPlugin(StanzaError())
-        ..enable('error');
-      final error = iq['error'] as XMLBase;
-      error['condition'] = 'feature-not-implemented';
-      error['text'] = 'No handlers registered';
-      iq.sendIQ();
-    }
+  /// Sets an error for this IQ stanza.
+  void makeError(ErrorStanza error) {
+    type = 'error';
+    this.error = error;
   }
 
+  /// Returns the name of the IQ stanza.
   @override
-  IQ error() {
-    super.error();
-    return this;
-  }
-
-  /// Overrides [reply] method, instead copies [IQ] with the overrided [copy]
-  /// method.
-  IQ replyIQ({bool clear = true}) {
-    final iq = super.reply<IQ>(copiedStanza: copy(), clear: clear);
-    iq['type'] = 'result';
-    return iq;
-  }
+  String get name => _name;
 
   @override
-  IQ copy({xml.XmlElement? element, XMLBase? parent, bool receive = false}) =>
-      IQ(
-        generateID: _generateID,
-        pluginMultiAttribute: pluginMultiAttribute,
-        overrides: overrides,
-        pluginTagMapping: pluginTagMapping,
-        pluginAttributeMapping: pluginAttributeMapping,
-        subInterfaces: subInterfaces,
-        boolInterfaces: boolInterfaces,
-        languageInterfaces: languageInterfaces,
-        pluginIterables: pluginIterables,
-        isExtension: isExtension,
-        includeNamespace: includeNamespace,
-        getters: getters,
-        setters: setters,
-        deleters: deleters,
-        receive: receive,
-        element: element,
-        parent: parent,
-      );
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is IQ &&
+          runtimeType == other.runtimeType &&
+          type == other.type &&
+          id == other.id &&
+          payload == other.payload &&
+          error == other.error &&
+          any == other.any;
+
+  @override
+  int get hashCode =>
+      type.hashCode ^
+      id.hashCode ^
+      payload.hashCode ^
+      error.hashCode ^
+      any.hashCode;
 }
