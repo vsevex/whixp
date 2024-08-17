d0d914ef7ce6c5150ca7d190f88de8b99459a78c
vsevex <vsevex@gmail.com>
2024-08-18 00:08:47 +0400

    refactor: Update _database.dart for improved code organization and functionality

--- lib/src/roster/_database.dart
+++ lib/src/roster/_database.dart
@@ -1,36 +1,36 @@
-part of 'manager.dart';
+// part of 'manager.dart';
 
-class _HiveDatabase {
-  factory _HiveDatabase() => _instance;
+// class _HiveDatabase {
+//   factory _HiveDatabase() => _instance;
 
-  _HiveDatabase._();
+//   _HiveDatabase._();
 
-  late Box<Map<dynamic, dynamic>> box;
+//   late Box<Map<dynamic, dynamic>> box;
 
-  static final _HiveDatabase _instance = _HiveDatabase._();
+//   static final _HiveDatabase _instance = _HiveDatabase._();
 
-  Future<void> initialize(String name, [String? path]) async =>
-      box = await Hive.openBox<Map<dynamic, dynamic>>(name, path: path);
+//   Future<void> initialize(String name, [String? path]) async =>
+//       box = await Hive.openBox<Map<dynamic, dynamic>>(name, path: path);
 
-  Map<dynamic, dynamic>? getState(String owner, String jid) {
-    final data = getJID(owner);
+//   Map<dynamic, dynamic>? getState(String owner, String jid) {
+//     final data = getJID(owner);
 
-    return data == null ? null : data[jid] as Map<dynamic, dynamic>;
-  }
+//     return data == null ? null : data[jid] as Map<dynamic, dynamic>;
+//   }
 
-  Map<dynamic, dynamic>? getJID(String owner) => box.get(owner);
+//   Map<dynamic, dynamic>? getJID(String owner) => box.get(owner);
 
-  Stream<BoxEvent> listenable() => box.watch();
+//   Stream<BoxEvent> listenable() => box.watch();
 
-  Future<void> updateData(
-    String owner,
-    String jid,
-    Map<dynamic, dynamic> data,
-  ) {
-    final existingData = getJID(owner);
-    if (existingData != null) {
-      existingData.addAll({jid: data});
-    }
-    return box.put(owner, existingData ?? {jid: data});
-  }
-}
+//   Future<void> updateData(
+//     String owner,
+//     String jid,
+//     Map<dynamic, dynamic> data,
+//   ) {
+//     final existingData = getJID(owner);
+//     if (existingData != null) {
+//       existingData.addAll({jid: data});
+//     }
+//     return box.put(owner, existingData ?? {jid: data});
+//   }
+// }
