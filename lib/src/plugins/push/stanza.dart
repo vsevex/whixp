30f8b6834e8714ac8492696de184d6d6b5de6893
vsevex <vsevex@gmail.com>
2024-08-18 00:08:13 +0400

    refactor: Update Push class to enable and disable push notifications

--- lib/src/plugins/push/stanza.dart
+++ lib/src/plugins/push/stanza.dart
@@ -1,44 +1,95 @@
 part of 'push.dart';
 
-class _EnablePush extends XMLBase {
-  _EnablePush({
-    super.pluginTagMapping,
-    super.pluginAttributeMapping,
-    super.element,
-    super.parent,
-  }) : super(
-          name: 'enable',
-          namespace: 'urn:xmpp:push:0',
-          pluginAttribute: 'enable',
-          interfaces: <String>{'node', 'jid'},
-        ) {
-    registerPlugin(Form());
+/// Represents an IQ stanza used to enable push notifications for a specific
+/// Jabber ID (JID) and node.
+///
+/// ```dart
+/// final xml = enableStanza.toXML();
+/// ```
+class Enable extends IQStanza {
+  const Enable(this.jid, this.node, {this.payload});
+
+  final JabberID jid;
+  final String node;
+  final Form? payload;
+
+  @override
+  xml.XmlElement toXML() {
+    final builder = WhixpUtils.makeGenerator();
+
+    builder.element(
+      name,
+      attributes: <String, String>{
+        'xmlns': namespace,
+        'jid': jid.toString(),
+        'node': node,
+      },
+    );
+
+    final element = builder.buildDocument().rootElement;
+    if (payload != null) element.children.add(payload!.toXML());
+
+    return element;
   }
 
   @override
-  _EnablePush copy({xml.XmlElement? element, XMLBase? parent}) {
-    return _EnablePush(
-      pluginTagMapping: pluginTagMapping,
-      pluginAttributeMapping: pluginTagMapping,
-      element: element,
-      parent: parent,
-    );
-  }
+  String get name => 'enable';
+
+  @override
+  String get namespace => 'urn:xmpp:push:0';
+
+  @override
+  String get tag => enableTag;
 }
 
-class _DisablePush extends XMLBase {
-  _DisablePush({
-    super.element,
-    super.parent,
-  }) : super(
-          name: 'disable',
-          namespace: 'urn:xmpp:push:0',
-          pluginAttribute: 'disable',
-          interfaces: <String>{'node', 'jid'},
-        );
+/// Represents an IQ stanza used to disable push notifications for a specific
+/// Jabber ID (JID).
+class Disable extends IQStanza {
+  const Disable(this.jid, {this.node});
+
+  final JabberID jid;
+  final String? node;
 
   @override
-  _DisablePush copy({xml.XmlElement? element, XMLBase? parent}) {
-    return _DisablePush(element: element, parent: parent);
+  xml.XmlElement toXML() {
+    final builder = WhixpUtils.makeGenerator();
+    final attributes = <String, String>{};
+
+    attributes['jid'] = jid.toString();
+    if (node?.isNotEmpty ?? false) attributes['node'] = node!;
+
+    builder.element(
+      name,
+      attributes: <String, String>{'xmlns': namespace}..addAll(attributes),
+    );
+
+    return builder.buildDocument().rootElement;
   }
+
+  factory Disable.fromXML(xml.XmlElement node) {
+    String? jid;
+    String? nod;
+
+    for (final attribute in node.attributes) {
+      switch (attribute.localName) {
+        case 'jid':
+          jid = attribute.value;
+        case 'node':
+          nod = attribute.value;
+        default:
+          break;
+      }
+    }
+
+    return Disable(JabberID(jid), node: nod);
+  }
+
+  @override
+  String get name => 'disable';
+
+  @override
+  String get namespace => 'urn:xmpp:push:0';
+
+  @override
+  String get tag => disableTag;
 }
