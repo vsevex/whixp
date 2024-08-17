832200cc0d2efd599aff95e9ec747fbda07dc12f
vsevex <vsevex@gmail.com>
2024-08-18 00:04:47 +0400

    refactor: Add SASLMechanisms class for handling SASL mechanisms in XMPP

--- lib/src/plugins/mechanisms/stanza/_response.dart
+++ lib/src/plugins/mechanisms/stanza/_response.dart
@@ -1,28 +1,31 @@
 part of '../feature.dart';
 
-class _Response extends StanzaBase {
-  _Response({super.transport})
-      : super(
-          name: 'response',
-          namespace: WhixpUtils.getNamespace('SASL'),
-          interfaces: {'value'},
-          pluginAttribute: 'response',
-          getters: <Symbol, dynamic Function(dynamic args, XMLBase base)>{
-            const Symbol('value'): (args, base) =>
-                WhixpUtils.atob(base.element!.innerText),
-          },
-          setters: <Symbol,
-              void Function(dynamic value, dynamic args, XMLBase base)>{
-            const Symbol('value'): (value, args, base) {
-              if ((value as String).isNotEmpty) {
-                base.element!.innerText = WhixpUtils.btoa(value);
-              } else {
-                base.element!.innerText = '=';
-              }
-            },
-          },
-          deleters: <Symbol, dynamic Function(dynamic args, XMLBase base)>{
-            const Symbol('value'): (args, base) => base.element!.innerText = '',
-          },
-        );
+/// Represents a response packet.
+///
+/// This packet is used to send a response during an authentication process.
+class SASLResponse with Packet {
+  /// Constructs a [SASLResponse] packet.
+  const SASLResponse({this.body});
+
+  /// The body of the response.
+  final String? body;
+
+  /// Constructs a [SASLResponse] packet from XML.
+  factory SASLResponse.fromXML(xml.XmlElement node) =>
+      SASLResponse(body: node.innerText);
+
+  @override
+  xml.XmlElement toXML() {
+    final element = WhixpUtils.xmlElement('response', namespace: _namespace);
+
+    element.children.add(
+      xml.XmlText((body?.isNotEmpty ?? false) ? WhixpUtils.btoa(body!) : '=')
+          .copy(),
+    );
+
+    return element;
+  }
+
+  @override
+  String get name => _response;
 }
