0997373fe7c53b003054aa7a9504f9cf6216ab2d
vsevex <vsevex@gmail.com>
2024-08-17 23:58:21 +0400

    refactor(xml): Update static.dart with XMPP stanza constants

---
+++ lib/src/_static.dart
@@ -0,0 +1,61 @@
+import 'package:whixp/src/utils/utils.dart';
+
+const String $errorStanza = 'error';
+const String $presence = 'presence';
+const String $bind = 'bind';
+const String $message = 'message';
+const String $handshake = 'handshake';
+const String $rosterQuery = 'query';
+const String $rosterItem = 'rosterItem';
+
+const String iqTypeError = "error";
+const String iqTypeGet = "get";
+const String iqTypeResult = "result";
+const String iqTypeSet = "set";
+
+const String messageTypeChat = "chat";
+const String messageTypeError = "error";
+const String messageTypeGroupchat = "groupchat";
+const String messageTypeHeadline = "headline";
+const String messageTypeNormal = "normal";
+
+const String presenceTypeError = "error";
+const String presenceTypeProbe = "probe";
+const String presenceTypeSubscribe = "subscribe";
+const String presenceTypeSubscribed = "subscribed";
+const String presenceTypeUnavailable = "unavailable";
+const String presenceTypeUnsubscribe = "unsubscribe";
+const String presenceTypeUnsubscribed = "unsubscribed";
+
+const String presenceShowChat = 'chat';
+const String presenceShowAway = 'away';
+const String presenceShowDnd = 'dnd';
+const String presenceShowXa = 'xa';
+
+const String errorAuth = 'auth';
+const String errorCancel = 'cancel';
+const String errorContinue = 'continue';
+const String errorModify = 'modify';
+const String errorWait = 'wait';
+
+String get discoInformationTag =>
+    '{${WhixpUtils.getNamespace('DISCO_INFO')}}query';
+String get discoItemsTag => '{${WhixpUtils.getNamespace('DISCO_ITEMS')}}query';
+String get bindTag => '{${WhixpUtils.getNamespace('BIND')}}bind';
+String get versionTag => '{jabber:iq:version}query';
+String get formsTag => '{${WhixpUtils.getNamespace('FORMS')}}x';
+String get tuneTag => '{http://jabber.org/protocol/tune}tune';
+String get moodTag => '{http://jabber.org/protocol/mood}mood';
+String get pubsubTag => '{${WhixpUtils.getNamespace('PUBSUB')}}pubsub';
+String get pubsubOwnerTag =>
+    '{${WhixpUtils.getNamespace('PUBSUB')}#owner}pubsub';
+String get pubsubEventTag =>
+    '{${WhixpUtils.getNamespace('PUBSUB')}#event}event';
+String get vCard4Tag => '{urn:ietf:params:xml:ns:vcard-4.0}vcard';
+String get vCardTag => '{vcard-temp}vCard';
+String get adhocCommandTag => '{http://jabber.org/protocol/commands}command';
+String get enableTag => '{urn:xmpp:push:0}enable';
+String get disableTag => '{urn:xmpp:push:0}disable';
+String get delayTag => '{urn:xmpp:delay}delay';
+String get stanzaIDTag => '{urn:xmpp:sid:0}stanza-id';
+String get originIDTag => '{urn:xmpp:sid:0}origin-id';
