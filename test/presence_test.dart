fb7d79f4d6de39f4042f726a5f753751ee5cfeea
vsevex <vsevex@gmail.com>
2024-08-17 23:58:06 +0400

    refactor(presence): Update presence_test.dart with improved test cases and imports

--- test/presence_test.dart
+++ test/presence_test.dart
@@ -1,31 +1,70 @@
 import 'package:test/test.dart';
 
+import 'package:whixp/src/_static.dart';
+import 'package:whixp/src/jid/jid.dart';
 import 'package:whixp/src/stanza/presence.dart';
 
-import 'test_base.dart';
-
 void main() {
-  group('Presence stanza test cases', () {
-    test('regression check presence["type"] = "dnd" show value working', () {
+  group('presence stanza test cases', () {
+    test('must generate presence and attach attributes', () {
       final presence = Presence();
-      presence['type'] = 'dnd';
-      check(presence, '<presence><show>dnd</show></presence>');
+
+      presence
+        ..type = presenceShowChat
+        ..from = JabberID('vsevex@localhost')
+        ..to = JabberID('alyosha@localhost')
+        ..id = 'someID';
+
+      final xml = presence.toXML();
+      expect(
+        xml.toString(),
+        '<presence to="alyosha@localhost" from="vsevex@localhost" type="chat" id="someID"/>',
+      );
+
+      final fromString = Presence.fromString(
+        '<presence type="chat" id="someID" from="vsevex@localhost" to="alyosha@localhost"/>',
+      );
+      expect(fromString.to, isNotNull);
+      expect(fromString.to, equals(JabberID('alyosha@localhost')));
+      expect(fromString.from, isNotNull);
+      expect(fromString.from, equals(JabberID('vsevex@localhost')));
+      expect(fromString.id, isNotNull);
+      expect(fromString.id, equals('someID'));
     });
 
-    test('properly manipulate presence type', () {
-      final presence = Presence();
-      presence['type'] = 'available';
-      check(presence, '<presence/>');
-      expect(presence['type'], 'available');
+    test('presence sub elements test case', () {
+      final presence = Presence(
+        show: presenceShowAway,
+        status: 'Sleeping',
+        priority: 10,
+      );
 
-      for (final showtype in {'away', 'chat', 'dnd', 'xa'}) {
-        presence['type'] = showtype;
-        check(presence, '<presence><show>$showtype</show></presence>');
-        expect(presence['type'], showtype);
-      }
+      presence
+        ..type = presenceShowChat
+        ..from = JabberID('vsevex@localhost')
+        ..to = JabberID('alyosha@localhost')
+        ..id = 'someID';
 
-      presence.delete('type');
-      check(presence, '<presence/>');
+      final xml = presence.toXML();
+      final fromXML = Presence.fromXML(xml);
+
+      expect(fromXML.show, isNotNull);
+      expect(fromXML.show, equals(presence.show));
+      expect(fromXML.status, isNotNull);
+      expect(fromXML.status, equals(presence.status));
+      expect(fromXML.priority, isNotNull);
+      expect(fromXML.priority, equals(presence.priority));
+    });
+
+    test('presence with error substanza', () {
+      final presence = Presence.fromString(
+        '<presence><error code="404" type="cancel"><item-not-found xmlns="urn:ietf:params:xml:ns:xmpp-stanzas"/></error></presence>',
+      );
+      final fromXML = Presence.fromXML(presence.toXML());
+
+      expect(fromXML.error, isNotNull);
+      expect(fromXML.error!.code, 404);
+      expect(fromXML.error!.type, errorCancel);
     });
   });
 }
