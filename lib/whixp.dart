6a299c561189b43b40b160083bdc5a584301abe8
vsevex <vsevex@gmail.com>
2024-08-18 00:09:01 +0400

    refactor: Update XMPP classes for improved code organization and functionality

--- lib/whixp.dart
+++ lib/whixp.dart
@@ -2,16 +2,18 @@
 /// Messaging and Presence Protocol) connections.
 library whixp;
 
+export 'src/client.dart';
+export 'src/enums.dart';
 export 'src/exception.dart';
 export 'src/jid/jid.dart';
 export 'src/log/log.dart';
 export 'src/plugins/plugins.dart';
+export 'src/reconnection.dart';
 export 'src/roster/manager.dart';
 export 'src/stanza/atom.dart';
 export 'src/stanza/error.dart';
 export 'src/stanza/iq.dart';
 export 'src/stanza/message.dart';
 export 'src/stanza/presence.dart';
-export 'src/stanza/roster.dart' hide RosterItem;
+export 'src/stanza/stanza.dart';
 export 'src/utils/utils.dart';
-export 'src/whixp.dart';
