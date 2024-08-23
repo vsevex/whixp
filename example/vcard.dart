449386f94eeb6a11201b6f0d7079612da59f8b79
vsevex <vsevex@gmail.com>
2024-08-24 01:48:21 +0400

    provide `vCard` example usage

---
+++ example/vcard.dart
@@ -0,0 +1,18 @@
+import 'package:whixp/whixp.dart';
+
+void main() {
+  final whixp = Whixp(
+    jabberID: 'vsevex@localhost/mobile',
+    password: 'passwd',
+    logger: Log(enableWarning: true, enableError: true, includeTimestamp: true),
+    internalDatabasePath: 'whixp',
+    reconnectionPolicy: RandomBackoffReconnectionPolicy(1, 3),
+  );
+
+  whixp
+    ..addEventHandler(
+      'streamNegotiated',
+      (_) => PubSub.retrieveVCard(JabberID('alyosha@localhost')),
+    )
+    ..connect();
+}
