fb647c2e1b843f7e283d09dfe08402d9bb3bb53d
vsevex <vsevex@gmail.com>
2024-08-27 17:17:37 +0400

    fix: remove pass from example

--- example/csi.dart
+++ example/csi.dart
@@ -3,7 +3,7 @@ import 'package:whixp/whixp.dart';
 void main() {
   final whixp = Whixp(
     jabberID: 'vsevex@localhost/desktop',
-    password: 'vesevu13',
+    password: 'passwd',
     logger: Log(enableWarning: true, enableError: true, includeTimestamp: true),
     internalDatabasePath: 'whixp',
     reconnectionPolicy: RandomBackoffReconnectionPolicy(1, 3),
