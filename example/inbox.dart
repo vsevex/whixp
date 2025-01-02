ca5593069976ed585770442fa65d5cb04f12d99d
vsevex <vsevex@gmail.com>
2025-01-02 12:05:37 +0400

    refactor: update jabberID and streamline event handler syntax in inbox.dart

--- example/inbox.dart
+++ example/inbox.dart
@@ -1,9 +1,8 @@
-import 'package:whixp/src/plugins/inbox/inbox.dart';
 import 'package:whixp/whixp.dart';
 
 void main() {
   final whixp = Whixp(
-    jabberID: 'asdf@localhost',
+    jabberID: 'vsevex@localhost/resource',
     password: 'passwd',
     logger: Log(
       enableWarning: true,
@@ -14,9 +13,7 @@ void main() {
   );
 
   whixp
-    ..addEventHandler('streamNegotiated', (_) {
-      return getInbox();
-    })
+    ..addEventHandler('streamNegotiated', (_) => getInbox())
     ..addEventHandler<Message>('message', (message) {
       final result = message?.get<InboxResult>();
       if (result?.isNotEmpty ?? false) {
@@ -27,7 +24,8 @@ void main() {
           }
           Log.instance.info("marked: ${forwarded?.actual?.isMarked}");
           Log.instance.info(
-              "box: ${stanza.box} \t archive: ${stanza.archive} \t mute: ${stanza.mute}");
+            "box: ${stanza.box} \t archive: ${stanza.archive} \t mute: ${stanza.mute}",
+          );
           Log.instance.info(
             "from: ${forwarded?.actual?.from?.username} \t to: ${forwarded?.actual?.to?.username}",
           );
