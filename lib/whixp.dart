ec2aeddc64b4e94a8f04630df96a3850c12413a9
vsevex <vsevex@gmail.com>
2026-01-28 12:59:08 +0400

    refactor: update library export
    Refine top-level whixp library export.

--- lib/whixp.dart
+++ lib/whixp.dart
@@ -1,12 +1,13 @@
 /// Serves as the foundation for establishing and managing XMPP (Extensible
 /// Messaging and Presence Protocol) connections.
-library whixp;
+library;
 
 export 'src/client.dart';
 export 'src/enums.dart';
 export 'src/exception.dart';
 export 'src/jid/jid.dart';
 export 'src/log/log.dart';
+export 'src/performance/metrics.dart';
 export 'src/plugins/plugins.dart';
 export 'src/reconnection.dart';
 export 'src/stanza/error.dart';
@@ -15,4 +16,5 @@ export 'src/stanza/iq.dart';
 export 'src/stanza/message.dart';
 export 'src/stanza/presence.dart';
 export 'src/stanza/stanza.dart';
+export 'src/transport.dart';
 export 'src/utils/utils.dart';
